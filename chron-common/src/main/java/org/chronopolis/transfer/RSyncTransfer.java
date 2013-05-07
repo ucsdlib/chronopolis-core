284ec5b29b8db1d5422c1552057a2ac3dd957ada
Michael Ritter <shake@umiacs.umd.edu>
2013-05-07 16:23:36 -0400

    Added new project for common files. Started it off w/ some transfer utils.

---
+++ chron-common/src/main/java/org/chronopolis/transfer/RSyncTransfer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chronopolis.transfer;
+
+import java.io.IOException;
+import org.chronopolis.exception.RSyncException;
+
+/**
+ *
+ * @author shake
+ */
+public class RSyncTransfer {
+
+    public static int rsyncTransfer(String user, String host, String remote, String local) throws IOException, InterruptedException {
+        // Taken from http://stackoverflow.com/questions/1246255/any-good-rsync-library-for-java
+        // Need to test/modify command 
+        // Currently uses passwordless SSH keys to login to sword
+        String[] cmd = new String[]{"rsync", "-r", user + "@" + host + ":" + remote, local};
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        Process p = pb.start();
+        int val = p.waitFor();
+        if (val != 0) {
+            throw new RSyncException("Exception during RSync; return code = " + val);
+        }
+        return val;
+    }
+    
+}
