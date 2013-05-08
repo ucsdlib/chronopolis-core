b2dd0ba30d4935496a9fe15233f10b56707bcaf5
Michael Ritter <shake@umiacs.umd.edu>
2013-05-08 14:32:19 -0400

    Removed spring elements, not sure if we want to use that yet.

--- chron-common/src/main/java/org/chronopolis/transfer/HttpsTransfer.java
+++ chron-common/src/main/java/org/chronopolis/transfer/HttpsTransfer.java
@@ -6,16 +6,14 @@ package org.chronopolis.transfer;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
+ * TODO: Stop requests that are sent w/ http
+ *       Move other code over and what not
+ *       
  *
  * @author shake
  */
-@Controller
-@RequestMapping("/somemapping")
 public class HttpsTransfer {
     /**
      *
