409d86a0274d60e1a7d628373acf9e3e8a1bc8f7
Michael Ritter <shake@umiacs.umd.edu>
2013-05-15 12:55:34 -0400

    Adding ingest/intake shell projects. These will be driven from the command line for early message flow testing.

---
+++ chron-ingest-shell/src/main/java/org/chronopolis/chron/ingest/shell/App.java
@@ -0,0 +1,13 @@
+package org.chronopolis.chron.ingest.shell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
