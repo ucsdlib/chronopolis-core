17bb67ded20dae327e8a8834e458c0b625601b05
Michael Ritter <shake@umiacs.umd.edu>
2014-02-05 16:21:09 -0500

    Tests

--- chron-common/src/main/java/org/chronopolis/common/digest/DigestUtil.java
+++ chron-common/src/main/java/org/chronopolis/common/digest/DigestUtil.java
@@ -1,9 +1,15 @@
 package org.chronopolis.common.digest;
 
+import edu.umiacs.ace.util.HashValue;
+import edu.umiacs.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -11,7 +17,7 @@ import java.security.NoSuchAlgorithmException;
  * Created by shake on 1/28/14.
  */
 public class DigestUtil {
-    private final Logger log = LoggerFactory.getLogger(DigestUtil.class);
+    private static final Logger log = LoggerFactory.getLogger(DigestUtil.class);
 
     /**
      * Digest the file and return it in hex format
@@ -21,12 +27,21 @@ public class DigestUtil {
      * @return string representation of the digest
      */
     public static String digest(Path file, String alg) {
-        MessageDigest md;
+        MessageDigest md = null;
         try {
             md = MessageDigest.getInstance(alg);
+            DigestInputStream dis = new DigestInputStream(Files.newInputStream(file), md);
+            int bufferSize = 1046576;
+            byte[] buf = new byte[bufferSize];
+            while ( dis.read(buf) >= 0 ) { }
         } catch (NoSuchAlgorithmException e) {
+            log.error("Error finding algorithm {}", alg, e);
+        } catch (IOException e) {
+            log.error("IO Error for {}", file, e);
         }
 
-        return null;
+        assert md != null;
+        return HashValue.asHexString(md.digest());
     }
+
 }
---
+++ chron-intake-shell/src/test/java/org/chronopolis/intake/processor/PackageIngestStatusResponseProcessorTest.java
@@ -0,0 +1,17 @@
+package org.chronopolis.intake.processor;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by shake on 2/5/14.
+ */
+public class PackageIngestStatusResponseProcessorTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testProcess() throws Exception {
+
+    }
+}
