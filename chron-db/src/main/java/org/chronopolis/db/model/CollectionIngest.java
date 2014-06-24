c1f800eb9dc13dc8a5b0c6f59134670c5143850c
Michael Ritter <shake@umiacs.umd.edu>
2014-06-24 16:07:51 -0400

    Add extra fields to keep track of during ingest

--- chron-db/src/main/java/org/chronopolis/db/model/CollectionIngest.java
+++ chron-db/src/main/java/org/chronopolis/db/model/CollectionIngest.java
@@ -9,14 +9,30 @@ import javax.persistence.Id;
  *
  * Created by shake on 4/9/14.
  */
-@Entity(name = "collection_ingest")
+@Entity(name = "collection")
 public class CollectionIngest {
+
     @Id
     @Column(name = "CORRELATION_ID")
     private String correlationId;
 
     @Column(name = "TO_DPN")
     private Boolean toDpn;
+
+    @Column
+    private String name;
+
+    @Column
+    private String depositor;
+
+    @Column
+    private Boolean tokensGenerated;
+
+    @Column
+    private String tokenDigest;
+
+    @Column
+    private String tagDigest;
 
     public String getCorrelationId() {
         return correlationId;
@@ -33,4 +49,45 @@ public class CollectionIngest {
     public void setToDpn(Boolean toDpn) {
         this.toDpn = toDpn;
     }
+
+    public String getTagDigest() {
+        return tagDigest;
+    }
+
+    public void setTagDigest(final String tagDigest) {
+        this.tagDigest = tagDigest;
+    }
+
+    public String getTokenDigest() {
+        return tokenDigest;
+    }
+
+    public void setTokenDigest(final String tokenDigest) {
+        this.tokenDigest = tokenDigest;
+    }
+
+    public Boolean getTokensGenerated() {
+        return tokensGenerated;
+    }
+
+    public void setTokensGenerated(final Boolean tokensGenerated) {
+        this.tokensGenerated = tokensGenerated;
+    }
+
+    public String getDepositor() {
+        return depositor;
+    }
+
+    public void setDepositor(final String depositor) {
+        this.depositor = depositor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
 }
