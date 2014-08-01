631a06212b98d20cc349671cb15e6ac3876029ed
Michael Ritter <shake@umiacs.umd.edu>
2014-08-01 17:03:03 -0400

    Add classes for supporting creation of bags from duraspace

---
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/batch/SnapshotJobManager.java
@@ -0,0 +1,78 @@
+package org.chronopolis.intake.duracloud.batch;
+
+import org.chronopolis.common.settings.ChronopolisSettings;
+import org.chronopolis.ingest.bagger.BagModel;
+import org.chronopolis.intake.duracloud.config.IntakeSettings;
+import org.chronopolis.intake.duracloud.model.DuracloudRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by shake on 7/29/14.
+ */
+public class SnapshotJobManager {
+    private final Logger log = LoggerFactory.getLogger(org.chronopolis.intake.duracloud.batch.SnapshotJobManager.class);
+
+    private ExecutorService executor;
+
+    private SnapshotProcessor processor;
+    private SnapshotWriter writer;
+    private IntakeSettings intakeSettings;
+
+    private HashMap<String, BagModel> models;
+
+    @Autowired
+    public SnapshotJobManager(SnapshotProcessor processor,
+                              SnapshotWriter writer,
+                              IntakeSettings intakeSettings) {
+        this.processor = processor;
+        this.writer = writer;
+        this.intakeSettings = intakeSettings;
+        this.executor = Executors.newSingleThreadExecutor();
+    }
+
+    public void addSnapshotJob(DuracloudRequest bag) {
+        log.trace("Adding job for bag {}", bag.getSnapshotID());
+        BagModel model = models.get(bag.getSnapshotID());
+        if (model == null) {
+            SnapshotThread thread = new SnapshotThread(bag);
+            executor.submit(thread);
+        } else {
+            log.error("Already started thread for snapshot " + bag.getSnapshotID());
+        }
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void destroy() throws Exception {
+        System.out.println("Shutting down thread pools");
+        executor.shutdown();
+
+        if (!executor.isTerminated()) {
+            executor.shutdownNow();
+        }
+
+        executor = null;
+    }
+
+    public class SnapshotThread implements Runnable {
+        private final DuracloudRequest bag;
+
+        public SnapshotThread(final DuracloudRequest bag) {
+            this.bag = bag;
+        }
+
+        @Override
+        public void run() {
+            SnapshotReader reader = new SnapshotReader(bag, intakeSettings);
+            BagModel model = reader.read();
+            models.put(bag.getSnapshotID(), model);
+            model = processor.process(model);
+            writer.write(model);
+        }
+    }
+}
---
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/batch/SnapshotReader.java
@@ -0,0 +1,108 @@
+package org.chronopolis.intake.duracloud.batch;
+
+import org.apache.log4j.Logger;
+import org.chronopolis.db.intake.StatusRepository;
+import org.chronopolis.db.intake.model.Status;
+import org.chronopolis.ingest.bagger.BagModel;
+import org.chronopolis.ingest.bagger.BagType;
+import org.chronopolis.ingest.bagger.IngestionType;
+import org.chronopolis.ingest.pkg.ChronPackage;
+import org.chronopolis.intake.duracloud.config.IntakeSettings;
+import org.chronopolis.intake.duracloud.model.BagDirectoryFilter;
+import org.chronopolis.intake.duracloud.model.DuracloudRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Created by shake on 7/29/14.
+ */
+public class SnapshotReader {
+    private final Logger log = Logger.getLogger(org.chronopolis.intake.duracloud.batch.SnapshotReader.class);
+
+    private final DuracloudRequest bag;
+    private final IntakeSettings intakeSettings;
+    private final DirectoryStream.Filter<Path> filter;
+    private StatusRepository statusRepository;
+
+    public SnapshotReader(DuracloudRequest bag, final IntakeSettings intakeSettings) {
+        this.bag = bag;
+        this.intakeSettings = intakeSettings;
+        this.filter = new BagDirectoryFilter();
+    }
+
+    public BagModel read() {
+        String base = intakeSettings.getDuracloudStage();
+        String bagId = bag.getSnapshotID();
+        String depositor = bag.getDepositor();
+        String collectionName = bag.getCollectionName();
+        ChronPackage pkg = new ChronPackage();
+
+        Set<File> tagFiles = new HashSet<>();
+
+        Path snapshot = Paths.get(base, bagId);
+        Path data = snapshot.resolve("data");
+
+        try (DirectoryStream<Path> stream =
+                     Files.newDirectoryStream(snapshot, filter)) {
+            for (Path p : stream) {
+                log.trace("Adding tag file " + p.toString());
+                pkg.addTagFile(p.toFile());
+            }
+        } catch (IOException e) {
+            log.error("IOException", e);
+            //return serverError("Error creating tag files");
+        }
+
+        pkg.setName(collectionName);
+        pkg.setDepositor(depositor);
+        pkg.getRootList().put(data.toFile(), true);
+        pkg.addTagFiles(tagFiles);
+        pkg.setProvidedManifest(true);
+
+        Status status = new Status(bagId, depositor, collectionName);
+        BagModel model = createBagModel(bagId,
+                pkg,
+                BagType.FILLED,
+                IngestionType.DPN,
+                false);
+
+
+        // TODO: How to store the bag model
+        // models.put(bagId, model);
+        statusRepository.save(status);
+
+        return model;
+    }
+
+    /**
+     * Create a new BagModel object
+     *
+     * @param pkg
+     * @param bagType
+     * @param ingestionType
+     * @param compressed
+     * @return the BagModel
+     */
+    private BagModel createBagModel(String bagId,
+                                    ChronPackage pkg,
+                                    BagType bagType,
+                                    IngestionType ingestionType,
+                                    Boolean compressed) {
+        BagModel model = new BagModel();
+        model.setBagId(bagId);
+        model.setChronPackage(pkg);
+        model.setBagType(bagType);
+        model.setIngestionType(ingestionType);
+        model.setCompression(compressed);
+        return model;
+    }
+
+}
