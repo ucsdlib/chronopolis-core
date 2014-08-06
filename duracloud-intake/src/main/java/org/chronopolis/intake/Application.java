d94a0a2584b857290cf2eb290747501fba0c8617
Michael Ritter <shake@umiacs.umd.edu>
2014-08-06 16:42:09 -0400

    update logback

--- duracloud-intake/src/main/java/org/chronopolis/intake/Application.java
+++ duracloud-intake/src/main/java/org/chronopolis/intake/Application.java
@@ -1,11 +1,9 @@
 package org.chronopolis.intake;
 
 import org.chronopolis.common.settings.ChronopolisSettings;
-import org.chronopolis.intake.config.IntakeConfiguration;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
 import org.chronopolis.intake.duracloud.config.JPAConfiguration;
 import org.chronopolis.intake.duracloud.config.JPASettings;
-import org.chronopolis.intake.rest.BagCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,18 +11,18 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * Created by shake on 8/1/14.
  */
+// TODO: Use packages or packageClasses?
 @ComponentScan(basePackageClasses = {ChronopolisSettings.class,
         IntakeSettings.class,
         JPASettings.class,
-        JPAConfiguration.class,
-        IntakeConfiguration.class,
-        BagCreator.class})
+        JPAConfiguration.class},
+        basePackages = {"org.chronopolis.intake.config",
+                        "org.chronopolis.intake.rest"})
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setLogStartupInfo(true);
         springApplication.run(args);
         //SpringApplication.exit(springApplication.run(args));
     }
