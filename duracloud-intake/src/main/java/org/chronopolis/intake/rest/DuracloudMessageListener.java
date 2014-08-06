c0419f4964de2942ad4d1ac13876e05fafe1ebc5
Michael Ritter <shake@umiacs.umd.edu>
2014-08-06 14:59:13 -0400

    Add a MessageListener for the duracloud intake service

---
+++ duracloud-intake/src/main/java/org/chronopolis/intake/rest/DuracloudMessageListener.java
@@ -0,0 +1,34 @@
+package org.chronopolis.intake.rest;
+
+import org.chronopolis.amqp.ChronMessageListener;
+import org.chronopolis.intake.processor.CollectionRestoreReplyProcessor;
+import org.chronopolis.intake.processor.PackageIngestCompleteProcessor;
+import org.chronopolis.messaging.MessageType;
+import org.chronopolis.messaging.base.ChronProcessor;
+
+/**
+ * Created by shake on 8/6/14.
+ */
+public class DuracloudMessageListener extends ChronMessageListener {
+
+    private final CollectionRestoreReplyProcessor collectionRestoreReplyProcessor;
+    private final PackageIngestCompleteProcessor packageIngestCompleteProcessor;
+
+    public DuracloudMessageListener(CollectionRestoreReplyProcessor collectionRestoreReplyProcessor,
+                                    PackageIngestCompleteProcessor packageIngestCompleteProcessor) {
+        this.collectionRestoreReplyProcessor = collectionRestoreReplyProcessor;
+        this.packageIngestCompleteProcessor = packageIngestCompleteProcessor;
+    }
+
+    @Override
+    public ChronProcessor getProcessor(final MessageType type) {
+        switch (type) {
+            case COLLECTION_RESTORE_REPLY:
+                return collectionRestoreReplyProcessor;
+            case PACKAGE_INGEST_COMPLETE:
+                return packageIngestCompleteProcessor;
+            default:
+                throw new RuntimeException("Unexpected message type! " + type);
+        }
+    }
+}
