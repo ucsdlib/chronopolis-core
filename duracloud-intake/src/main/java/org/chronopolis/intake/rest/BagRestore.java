2edbba70911a9f2abcb9ced78977c7f6eb0f9df8
Michael Ritter <shake@umiacs.umd.edu>
2014-08-07 13:13:01 -0400

    Re-enable the restore request mapping

--- duracloud-intake/src/main/java/org/chronopolis/intake/rest/BagRestore.java
+++ duracloud-intake/src/main/java/org/chronopolis/intake/rest/BagRestore.java
@@ -1,7 +1,13 @@
 package org.chronopolis.intake.rest;
 
 import org.chronopolis.amqp.ChronProducer;
+import org.chronopolis.amqp.RoutingKey;
+import org.chronopolis.db.intake.StatusRepository;
+import org.chronopolis.db.intake.model.Status;
+import org.chronopolis.messaging.base.ChronMessage;
 import org.chronopolis.messaging.factory.MessageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/restore")
 public class BagRestore {
+    private static final Logger log = LoggerFactory.getLogger(BagRestore.class);
 
     @Autowired
     private ChronProducer producer;
@@ -23,22 +30,21 @@ public class BagRestore {
     @Autowired
     private MessageFactory messageFactory;
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public ResponseEntity restore(@PathVariable("id") String snapshotId) {
-        return new ResponseEntity(HttpStatus.OK);
-    }
+    @Autowired
+    private StatusRepository statusRepository;
 
-    /*
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public ResponseEntity restore(@PathVariable("id") String snapshotId) {
         // grab the depositor and collection name from the bagstatus
         // and forward that through to the ingest service
-        BagStatus status = statusAccessor.get(snapshotId);
+        Status status = statusRepository.findByBagId(snapshotId);
         ResponseEntity entity;
 
         if (status == null) {
+            log.info("Bag {} not found", snapshotId);
             entity = new ResponseEntity(HttpStatus.NOT_FOUND);
         } else if (status.isReplicated()) {
+            log.info("Bag {} found and is replicated", snapshotId);
             entity = new ResponseEntity(HttpStatus.OK);
             ChronMessage message = messageFactory.collectionRestoreRequestMessage(
                     status.getCollectionName(),
@@ -47,11 +53,11 @@ public class BagRestore {
 
             producer.send(message, RoutingKey.INGEST_BROADCAST.asRoute());
         } else {
+            log.info("Bag {} found and is not replicated", snapshotId);
             entity = new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
         return entity;
     }
-    */
 
 }
