24e963224ac0a6673c45c952f1b057508048bd19
Michael Ritter <shake@umiacs.umd.edu>
2014-08-07 15:56:40 -0400

    Stub processors for new collection restore messages

---
+++ replication-shell/src/main/java/org/chronopolis/replicate/processor/CollectionRestoreLocationProcessor.java
@@ -0,0 +1,14 @@
+package org.chronopolis.replicate.processor;
+
+import org.chronopolis.messaging.base.ChronMessage;
+import org.chronopolis.messaging.base.ChronProcessor;
+
+/**
+ * Created by shake on 8/7/14.
+ */
+public class CollectionRestoreLocationProcessor implements ChronProcessor {
+    @Override
+    public void process(final ChronMessage chronMessage) {
+
+    }
+}
