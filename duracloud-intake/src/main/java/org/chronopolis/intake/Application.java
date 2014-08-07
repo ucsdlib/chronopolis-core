3e1d09e8012a52fe360a9a9f92ac32bc1d5f8e11
Michael Ritter <shake@umiacs.umd.edu>
2014-08-07 13:41:10 -0400

    exitable main method

--- duracloud-intake/src/main/java/org/chronopolis/intake/Application.java
+++ duracloud-intake/src/main/java/org/chronopolis/intake/Application.java
@@ -6,7 +6,12 @@ import org.chronopolis.intake.duracloud.config.JPAConfiguration;
 import org.chronopolis.intake.duracloud.config.JPASettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by shake on 8/1/14.
@@ -21,9 +26,28 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class Application {
 
+    private static String readLine() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            return reader.readLine();
+        } catch (IOException ex) {
+            throw new RuntimeException("Can't read from STDIN");
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.run(args);
+        ApplicationContext ctx = springApplication.run(args);
+        System.out.println("Enter 'q' to exit");
+
+        boolean done = false;
+        while (!done) {
+            if ("q".equalsIgnoreCase(readLine())) {
+                done = true;
+                SpringApplication.exit(ctx);
+            }
+        }
+
         //SpringApplication.exit(springApplication.run(args));
     }
 
