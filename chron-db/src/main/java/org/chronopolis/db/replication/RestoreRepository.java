8c2f51a39639fd134bf8c499c0ecf840c967cccc
Michael Ritter <shake@umiacs.umd.edu>
2014-08-08 13:14:20 -0400

    Add method to find restore requests based on correlation id

--- chron-db/src/main/java/org/chronopolis/db/replication/RestoreRepository.java
+++ chron-db/src/main/java/org/chronopolis/db/replication/RestoreRepository.java
@@ -2,9 +2,14 @@ package org.chronopolis.db.replication;
 
 import org.chronopolis.db.replication.model.RestoreRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by shake on 8/8/14.
  */
+@Transactional
 public interface RestoreRepository extends CrudRepository<RestoreRequest, String> {
+
+    RestoreRequest findByCorrelationId(String correlationId);
+
 }
