f1d20c363ef0f62c72914a842dd787223b58e953
Michael Ritter <shake@umiacs.umd.edu>
2014-08-08 13:13:21 -0400

    Fill out entity

--- chron-db/src/main/java/org/chronopolis/db/replication/model/RestoreRequest.java
+++ chron-db/src/main/java/org/chronopolis/db/replication/model/RestoreRequest.java
@@ -1,10 +1,44 @@
 package org.chronopolis.db.replication.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * Created by shake on 8/8/14.
  */
 @Entity
 public class RestoreRequest {
+
+    @Id
+    @Column
+    String correlationId;
+
+    @Column
+    String directory;
+
+
+    public RestoreRequest() {
+    }
+
+    public RestoreRequest(final String directory, final String correlationId) {
+        this.directory = directory;
+        this.correlationId = correlationId;
+    }
+
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(final String correlationId) {
+        this.correlationId = correlationId;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(final String directory) {
+        this.directory = directory;
+    }
 }
