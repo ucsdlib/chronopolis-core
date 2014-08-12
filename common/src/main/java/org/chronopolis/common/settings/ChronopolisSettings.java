1cd5ab475c11346b075869bdde13fdeed77cce39
Michael Ritter <shake@umiacs.umd.edu>
2014-08-12 14:15:00 -0400

    Add documentation

--- common/src/main/java/org/chronopolis/common/settings/ChronopolisSettings.java
+++ common/src/main/java/org/chronopolis/common/settings/ChronopolisSettings.java
@@ -26,6 +26,13 @@ public class ChronopolisSettings {
     @Value("${chron.restore:/scratch1/restore}")
     private String restore;
 
+    /**
+     * Get the node name where the chronopolis service is running at
+     *
+     * i.e.: ncar, sdsc, umiacs
+     *
+     * @return
+     */
     public String getNode() {
         return node;
     }
@@ -34,6 +41,11 @@ public class ChronopolisSettings {
         this.node = node;
     }
 
+    /**
+     * Get the stage where bags are held
+     *
+     * @return
+     */
     public String getBagStage() {
         return bagStage;
     }
@@ -42,6 +54,11 @@ public class ChronopolisSettings {
         this.bagStage = bagStage;
     }
 
+    /**
+     * Get the stage where tokens are held
+     *
+     * @return
+     */
     public String getTokenStage() {
         return tokenStage;
     }
@@ -50,6 +67,11 @@ public class ChronopolisSettings {
         this.tokenStage = tokenStage;
     }
 
+    /**
+     * Get the location where bags are preserved
+     *
+     * @return
+     */
     public String getPreservation() {
         return preservation;
     }
@@ -58,6 +80,11 @@ public class ChronopolisSettings {
         this.preservation = preservation;
     }
 
+    /**
+     * Get the stage where restorations will be put
+     *
+     * @return
+     */
     public String getRestore() {
         return restore;
     }
