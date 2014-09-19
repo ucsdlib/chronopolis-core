68098fcb470289197c9968d8f3762dec1f6a6a3d
Michael Ritter <shake@umiacs.umd.edu>
2014-09-19 17:22:08 -0400

    Send an AMQP message when finished bagging

--- duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/batch/SnapshotTasklet.java
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/batch/SnapshotTasklet.java
@@ -1,12 +1,16 @@
 package org.chronopolis.intake.duracloud.batch;
 
 import org.chronopolis.amqp.ChronProducer;
+import org.chronopolis.amqp.RoutingKey;
+import org.chronopolis.common.digest.Digest;
 import org.chronopolis.common.mail.MailUtil;
 import org.chronopolis.ingest.bagger.IngestionType;
+import org.chronopolis.ingest.pkg.ChronPackage;
 import org.chronopolis.ingest.pkg.ManifestBuilder;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
 import org.chronopolis.intake.duracloud.model.DuracloudRequest;
 import org.chronopolis.messaging.factory.MessageFactory;
+import org.chronopolis.messaging.pkg.PackageReadyMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
@@ -14,6 +18,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -68,6 +73,23 @@ public class SnapshotTasklet implements Tasklet {
                 Charset.defaultCharset()));
         builder.newScanPackage();
 
+
+        ChronPackage chronPackage = builder.getPackage();
+        Digest digest = Digest.fromString(chronPackage.getMessageDigest());
+        long size = chronPackage.getTotalSize();
+        Path saveFile = Paths.get(settings.getBagStage(),
+                                  depositor,
+                                  chronPackage.getSaveName());
+
+        PackageReadyMessage packageReadyMessage = messageFactory.packageReadyMessage(
+                depositor,
+                digest,
+                saveFile.getFileName().toString(), // We only want the relative path
+                collectionName,                    // (ingest may have a different mount)
+                (int) size
+        );
+
+        producer.send(packageReadyMessage, RoutingKey.INGEST_BROADCAST.asRoute());
 
         return RepeatStatus.FINISHED;
     }
