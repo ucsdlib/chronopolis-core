4e3c67cf6e10b0a4fe5a4faf53102b2d647fbc23
Michael Ritter <shake@umiacs.umd.edu>
2014-11-17 15:46:00 -0500

    add entity for a node in chronopolis

---
+++ ingest-rest/src/main/java/org/chronopolis/ingest/model/Node.java
@@ -0,0 +1,52 @@
+package org.chronopolis.ingest.model;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+/**
+ * Created by shake on 11/17/14.
+ */
+@Entity
+public class Node {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(mappedBy = "node")
+    private Set<ReplicationAction> replications;
+
+    @JsonIgnore
+    public String password;
+    public String username;
+
+    // For JPA
+    public Node() {
+    }
+
+    public Node(final String password, final String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<ReplicationAction> getReplications() {
+        return replications;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
