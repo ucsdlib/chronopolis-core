51528b7970f2ab2a1c09836c49c90265c493bfd5
shake <shake@umiacs.umd.edu>
2014-11-18 15:51:58 -0500

    add principal in order to get user info

--- ingest-rest/src/main/java/org/chronopolis/ingest/api/StagingController.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/StagingController.java
@@ -5,12 +5,16 @@ import org.chronopolis.ingest.IngestSettings;
 import org.chronopolis.ingest.model.Bag;
 import org.chronopolis.ingest.model.IngestRequest;
 import org.chronopolis.ingest.repository.BagRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * Created by shake on 11/5/14.
@@ -19,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/staging")
 public class StagingController {
 
+    Logger log = LoggerFactory.getLogger(StagingController.class);
+
     @Autowired
     BagRepository bagRepository;
 
@@ -26,17 +32,17 @@ public class StagingController {
     IngestSettings ingestSettings;
 
     @RequestMapping(value = "bags", method = RequestMethod.GET)
-    public Iterable<Bag> getBags() {
+    public Iterable<Bag> getBags(Principal principal) {
         return bagRepository.findAll();
     }
 
     @RequestMapping(value = "bags/{bag-id}", method = RequestMethod.GET)
-    public Bag getBag(@PathVariable("bag-id") Long bagId) {
+    public Bag getBag(Principal principal, @PathVariable("bag-id") Long bagId) {
         return bagRepository.findOne(bagId);
     }
 
     @RequestMapping(value = "bags", method = RequestMethod.PUT)
-    public String stageBag(@RequestBody IngestRequest request) {
+    public String stageBag(Principal principal, @RequestBody IngestRequest request) {
         ChronPackager packager = new ChronPackager(request.getName(),
                 request.getFileName(),
                 request.getDepositor(),
