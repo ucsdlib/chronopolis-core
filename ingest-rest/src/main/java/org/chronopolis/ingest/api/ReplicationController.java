e90ae854ae5c540684fdbd2358af613bcfffd5aa
shake <shake@umiacs.umd.edu>
2014-11-18 15:51:27 -0500

    add replication api methods

--- ingest-rest/src/main/java/org/chronopolis/ingest/api/ReplicationController.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/ReplicationController.java
@@ -1,12 +1,16 @@
 package org.chronopolis.ingest.api;
 
 import org.chronopolis.ingest.model.ReplicationAction;
+import org.chronopolis.ingest.repository.ReplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.security.Principal;
+import java.util.Collection;
 
 /**
  * Created by shake on 11/5/14.
@@ -15,11 +19,23 @@ import java.util.List;
 @RequestMapping("/api/staging")
 public class ReplicationController {
 
-    public static List<ReplicationAction> currentActions = new ArrayList<>();
+    @Autowired
+    ReplicationRepository replicationRepository;
 
-    @RequestMapping(value = "/actions")
-    public String updateAction(@RequestBody ReplicationAction action) {
-        return "action-id";
+    @RequestMapping(value = "/replications", method = RequestMethod.PUT)
+    public String replications(Principal principal, @RequestBody ReplicationAction action) {
+        replicationRepository.save(action);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/replications", method = RequestMethod.GET)
+    public Collection<ReplicationAction> replications(Principal principal) {
+        return replicationRepository.findByNodeUsername(principal.getName());
+    }
+
+    @RequestMapping(value = "/replications/{id}")
+    public ReplicationAction findReplication(@PathVariable("id") Long actionId) {
+        return replicationRepository.findOne(actionId);
     }
 
 }
