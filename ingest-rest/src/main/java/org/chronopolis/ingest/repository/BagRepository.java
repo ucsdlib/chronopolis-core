47079745ccbd603c634f35d6b5308ef2e413e1fc
Michael Ritter <shake@umiacs.umd.edu>
2014-11-20 15:02:09 -0500

    update findByStatus to return a collection

--- ingest-rest/src/main/java/org/chronopolis/ingest/repository/BagRepository.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/repository/BagRepository.java
@@ -4,6 +4,8 @@ import org.chronopolis.ingest.model.Bag;
 import org.chronopolis.ingest.model.BagStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 /**
  * Created by shake on 11/6/14.
  */
@@ -12,6 +14,6 @@ public interface BagRepository extends JpaRepository<Bag, Long> {
     // Bag findById(Long id);
     Bag findByNameAndDepositor(String name, String depositor);
 
-    Bag findByStatus(BagStatus status);
+    Collection<Bag> findByStatus(BagStatus status);
 
 }
