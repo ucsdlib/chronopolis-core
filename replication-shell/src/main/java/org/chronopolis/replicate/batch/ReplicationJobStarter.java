4c7afb5dee4540dd79eeda11f592499e4a925f37
Michael Ritter <shake@umiacs.umd.edu>
2014-12-01 16:54:32 -0500

    add class to control building and launching of jobs

---
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/ReplicationJobStarter.java
@@ -0,0 +1,121 @@
+package org.chronopolis.replicate.batch;
+
+import org.chronopolis.amqp.ChronProducer;
+import org.chronopolis.common.ace.AceService;
+import org.chronopolis.common.ace.GsonCollection;
+import org.chronopolis.common.mail.MailUtil;
+import org.chronopolis.messaging.collection.CollectionInitCompleteMessage;
+import org.chronopolis.messaging.collection.CollectionInitMessage;
+import org.chronopolis.messaging.factory.MessageFactory;
+import org.chronopolis.replicate.ReplicationNotifier;
+import org.chronopolis.replicate.config.ReplicationSettings;
+import org.chronopolis.rest.models.Replication;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+
+/**
+ * Created by shake on 12/1/14.
+ */
+public class ReplicationJobStarter {
+
+
+    private final ChronProducer producer;
+    private final MessageFactory messageFactory;
+    private final ReplicationSettings settings;
+    private final MailUtil mailUtil;
+    private final AceService aceService;
+
+    private final ReplicationStepListener replicationStepListener;
+    private JobLauncher jobLauncher;
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+
+
+    public ReplicationJobStarter(final ChronProducer producer,
+                                 final MessageFactory messageFactory,
+                                 final ReplicationSettings replicationSettings,
+                                 final MailUtil mailUtil,
+                                 final AceService aceService,
+                                 final ReplicationStepListener replicationStepListener,
+                                 final JobLauncher jobLauncher,
+                                 final JobBuilderFactory jobBuilderFactory,
+                                 final StepBuilderFactory stepBuilderFactory) {
+        this.producer = producer;
+        this.messageFactory = messageFactory;
+        this.settings = replicationSettings;
+        this.mailUtil = mailUtil;
+        this.aceService = aceService;
+        this.replicationStepListener = replicationStepListener;
+        this.jobLauncher = jobLauncher;
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+
+    public void addJobFromMessage(CollectionInitMessage msg) {
+        String depositor = msg.getDepositor();
+        String collection = msg.getCollection();
+
+        // check to see if we already have the collection
+        // if we don't, replicate it
+        // if we do, just sent an init complete message
+        GsonCollection gsonCollection = aceService.getCollectionByName(collection, depositor);
+        if (gsonCollection == null) {
+            ReplicationNotifier notifier = new ReplicationNotifier(msg);
+            Job job = jobBuilderFactory.get("collection-replicate")
+                    .start(stepBuilderFactory.get("token-replicate")
+                        .tasklet(new TokenDownloadStep(settings, msg, notifier))
+                        .listener(replicationStepListener)
+                        .build())
+                    .next(stepBuilderFactory.get("bag-replicate")
+                        .tasklet(new BagDownloadStep(settings, msg, notifier))
+                        .listener(replicationStepListener)
+                        .build())
+                    .next(stepBuilderFactory.get("ace-register")
+                        .tasklet(new AceRegisterStep(aceService, settings, msg, notifier))
+                        .listener(replicationStepListener)
+                        .build())
+                    .next(stepBuilderFactory.get("replication-success")
+                        .tasklet(new ReplicationSuccessStep(producer, messageFactory, mailUtil, settings, notifier))
+                        .listener(replicationStepListener)
+                        .build())
+                    .build();
+
+            try {
+                jobLauncher.run(job, new JobParametersBuilder()
+                        .addString("depositor", depositor)
+                        .addString("collection", collection)
+                        .addString("token-store-location", msg.getTokenStore())
+                        .addString("token-store-digest", msg.getTokenStoreDigest())
+                        .addString("bag-location", msg.getBagLocation())
+                        .addString("tag-manifest-digest", msg.getTagManifestDigest())
+                        .addString("correlation-id", msg.getCorrelationId())
+                        .toJobParameters());
+            } catch (JobExecutionAlreadyRunningException e) {
+                e.printStackTrace();
+            } catch (JobRestartException e) {
+                e.printStackTrace();
+            } catch (JobInstanceAlreadyCompleteException e) {
+                e.printStackTrace();
+            } catch (JobParametersInvalidException e) {
+                e.printStackTrace();
+            }
+        } else {
+            CollectionInitCompleteMessage reply =
+                    messageFactory.collectionInitCompleteMessage(msg.getCorrelationId());
+            producer.send(reply, msg.getReturnKey());
+        }
+    }
+
+    public void addJobFromRestful(Replication replication) {
+
+    }
+
+}
