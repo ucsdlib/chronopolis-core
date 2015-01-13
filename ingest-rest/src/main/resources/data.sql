b4a35b96e85397e11a69976a795a2a8822a08e59
Michael Ritter <shake@umiacs.umd.edu>
2015-01-13 16:24:54 -0500

    use the schema to populate our db tables

--- ingest-rest/src/main/resources/data.sql
+++ ingest-rest/src/main/resources/data.sql
@@ -4,6 +4,11 @@ insert into users (username, password, enabled) values ('ncar', 'ncar', true);
 insert into users (username, password, enabled) values ('sdsc', 'sdsc', true);
 insert into users (username, password, enabled) values ('ucsd', 'ucsd', true);
 
+insert into node (id, enabled, password, username) VALUES (DEFAULT, true, 'umiacs', 'umiacs');
+insert into node (id, enabled, password, username) VALUES (DEFAULT, true, 'ncar', 'ncar');
+insert into node (id, enabled, password, username) VALUES (DEFAULT, true, 'sdsc', 'sdsc');
+insert into node (id, enabled, password, username) VALUES (DEFAULT, true, 'ucsd', 'ucsd');
+
 insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
 insert into authorities (username, authority) values ('umiacs', 'ROLE_USER');
 insert into authorities (username, authority) values ('ncar', 'ROLE_USER');
