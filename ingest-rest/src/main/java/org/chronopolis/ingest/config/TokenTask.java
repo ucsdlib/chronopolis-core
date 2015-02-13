79f14f7f07cdcf55cc41f917144254fe7012b763
Michael Ritter <shake@umiacs.umd.edu>
2015-02-13 16:35:22 -0500

    add a method to write tokens to a file after tokenization

--- ingest-rest/src/main/java/org/chronopolis/ingest/config/TokenTask.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/config/TokenTask.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Sets;
 import org.chronopolis.common.ace.Tokenizer;
 import org.chronopolis.ingest.IngestSettings;
 import org.chronopolis.ingest.TokenCallback;
+import org.chronopolis.ingest.TokenWriter;
 import org.chronopolis.ingest.repository.BagRepository;
 import org.chronopolis.ingest.repository.TokenRepository;
 import org.chronopolis.rest.models.AceToken;
@@ -17,9 +18,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 /**
  * Created by shake on 2/6/2015.
@@ -58,13 +65,13 @@ public class TokenTask {
             // Setup everything we need
             TokenCallback callback = new TokenCallback(tokenRepository, bag);
             Path toBag = Paths.get(settings.getBagStage(), bag.getLocation());
-            Tokenizer tokenizer = new Tokenizer(toBag, bag.getFixityAlgorithm(), callback);
 
             // We have 3 states we check for:
             // * if there are no tokens, start from the beginning
             // * if there are less tokens than the number of files in the bag, do a partial tokenization
             // * if tokenization is complete, update the status of the bag
             if (tokens.size() == 0) {
+                Tokenizer tokenizer = new Tokenizer(toBag, bag.getFixityAlgorithm(), callback);
                 boolean complete = true;
                 try {
                     tokenizer.tokenize(Sets.<Path>newHashSet());
@@ -76,14 +83,68 @@ public class TokenTask {
             } else if (tokens.size() < bag.getTotalFiles()) {
 
             } else if (tokens.size() == bag.getTotalFiles()) {
-                log.info("Updating status of {}", bag.getName());
-                bag.setStatus(BagStatus.TOKENIZED);
+                // TODO: May want to decouple this
+                log.info("Writing tokens for bag {}", bag.getID());
+                boolean written = writeTokens(bag, tokens);
+
+                if (written) {
+                    log.info("Updating status of {}", bag.getName());
+                    bag.setStatus(BagStatus.TOKENIZED);
+                }
+
+                repository.save(bag);
             }
 
             log.debug("Finished tokenizing {}", bag.getName());
         }
         tokening = false;
 
+    }
+
+    private Set<Path> filter(Collection<AceToken> tokens) {
+        Set<Path> filter = Sets.newHashSet();
+        for (final AceToken token : tokens) {
+            filter.add(Paths.get(token.getFilename()));
+        }
+        return filter;
+    }
+
+    /**
+     * Write a token to a file identified by the bag name and date
+     *
+     * @param bag
+     * @param tokens
+     * @return
+     */
+    private boolean writeTokens(Bag bag, Collection<AceToken> tokens) {
+        Path stage = Paths.get(settings.getTokenStage());
+        Path dir = stage.resolve(bag.getDepositor());
+        if (!dir.toFile().exists()) {
+            dir.toFile().mkdirs();
+        }
+
+        String filename = bag.getName() + new Date();
+        Path store = dir.resolve(filename);
+        try (OutputStream os = Files.newOutputStream(store, CREATE)) {
+            String ims = "ims.umiacs.umd.edu";
+            TokenWriter writer = new TokenWriter(os, ims);
+
+            for (AceToken token : tokens) {
+                writer.startToken(token);
+                writer.addIdentifier(token.getFilename());
+                writer.writeTokenEntry();
+            }
+
+            // The stream will close on it's own, but call this anyways
+            writer.close();
+        } catch (IOException ex) {
+            log.error("Error writing manifest {} ", ex);
+            return false;
+        }
+        log.info("Finished writing tokens");
+        bag.setTokenLocation(stage.relativize(store).toString());
+
+        return true;
     }
 
 }
