50dc788a6e7afe594d5f5fcb6e3adf74adeefbbd
Michael Ritter <shake@umiacs.umd.edu>
2015-02-23 15:03:36 -0500

    create different services depending on which profile we are in

--- replication-shell/src/main/java/org/chronopolis/replicate/ReplicationConsumer.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/ReplicationConsumer.java
@@ -4,6 +4,7 @@ import org.chronopolis.db.common.model.RestoreRequest;
 import org.chronopolis.replicate.batch.ReplicationJobStarter;
 import org.chronopolis.replicate.config.ReplicationConfig;
 import org.chronopolis.replicate.config.ReplicationSettings;
+import org.chronopolis.replicate.service.ReplicationService;
 import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.models.Replication;
 import org.chronopolis.rest.models.ReplicationStatus;
@@ -28,108 +29,25 @@ import java.util.List;
 @Component
 @ComponentScan(basePackageClasses = {
         ReplicationConfig.class,
-        ReplicationSettings.class
+        ReplicationSettings.class,
+        ReplicationService.class
 }, basePackages = {
         "org.chronopolis.common.settings"
 })
 @EntityScan(basePackageClasses = RestoreRequest.class)
 @EnableAutoConfiguration
 public class ReplicationConsumer implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(ReplicationConsumer.class);
 
     @Autowired
-    ReplicationSettings settings;
-
-    @Autowired
-    IngestAPI ingestAPI;
-
-    @Autowired
-    ReplicationJobStarter jobStarter;
-
-    private static String readLine() {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            return reader.readLine();
-        } catch (IOException ex) {
-            throw new RuntimeException("Can't read from STDIN");
-        }
-    }
+    ReplicationService service;
 
     public static void main(String[] args) {
-        SpringApplication.exit(SpringApplication.run(ReplicationConsumer.class, args));
-    }
-
-    private OPTION inputOption() {
-        OPTION option = OPTION.UNKNOWN;
-        while (option.equals(OPTION.UNKNOWN)) {
-            StringBuilder sb = new StringBuilder("Enter Option: ");
-            String sep = " | ";
-            for (OPTION value : OPTION.values()) {
-                if (!value.equals(OPTION.UNKNOWN)) {
-                    sb.append(value.name());
-                    sb.append(" [");
-                    sb.append(value.name().charAt(0));
-                    sb.append("]");
-                    sb.append(sep);
-                }
-            }
-
-            //The one difference, mwahhaha
-            sb.replace(sb.length() - sep.length(), sb.length(), " -> ");
-            System.out.println(sb.toString());
-            option = OPTION.fromString(readLine().trim());
-        }
-        return option;
-    }
-
-    private void query(ReplicationStatus status, boolean update) {
-        List<Replication> replications = ingestAPI.getReplications(status);
-        log.debug("Found {} replications", replications.size());
-
-        for (Replication replication : replications) {
-            log.info("Starting job for replication id {}", replication.getReplicationID());
-            if (update) {
-                log.info("Updating replication");
-                replication.setStatus(ReplicationStatus.STARTED);
-                ingestAPI.updateReplication(replication.getReplicationID(), replication);
-            }
-            jobStarter.addJobFromRestful(replication);
-        }
+        SpringApplication.run(ReplicationConsumer.class, args);
     }
 
     @Override
     public void run(final String... strings) throws Exception {
-        boolean done = false;
-        while (!done) {
-            OPTION option = inputOption();
-            if (option.equals(OPTION.RESTFUL_QUERY)) {
-                log.info("Query for active replications");
-                query(ReplicationStatus.STARTED, false);
-
-                log.info("Query for new replications");
-                query(ReplicationStatus.PENDING, true);
-            } else if (option.equals(OPTION.QUIT)) {
-                log.info("Quitting");
-                done = true;
-            }
-
-        }
+        service.replicate();
     }
 
-    private enum OPTION {
-        RESTFUL_QUERY, QUIT, UNKNOWN;
-
-        private static OPTION fromString(String text) {
-            switch (text) {
-                case "R":
-                case "r":
-                    return RESTFUL_QUERY;
-                case "Q":
-                case "q":
-                    return QUIT;
-                default:
-                    return UNKNOWN;
-            }
-        }
-    }
 }
