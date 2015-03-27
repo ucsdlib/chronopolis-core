ed3e099501ab8d9661b9313dee907c684b946f43
Michael Ritter <shake@umiacs.umd.edu>
2015-03-27 13:47:43 -0400

    create default admin role

--- ingest-rest/src/main/java/org/chronopolis/ingest/config/ProductionConfig.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/config/ProductionConfig.java
@@ -1,17 +1,13 @@
 package org.chronopolis.ingest.config;
 
-import org.chronopolis.ingest.service.IngestService;
-import org.chronopolis.ingest.service.ProductionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
@@ -33,8 +29,10 @@ public class ProductionConfig {
     private final Logger log = LoggerFactory.getLogger(ProductionConfig.class);
 
     private static final String SELECT_USERNAMES = "select username from users";
-    private static final String INSERT_ADMIN = "insert into users (username, password, enabled) values (?, ?, ?)";
-    private static final String DEFAULT_ADMIN = "admin";
+    private static final String INSERT_ADMIN     = "insert into users (username, password, enabled) values (?, ?, ?)";
+    private static final String INSERT_AUTHORITY = "insert into authorities (username, authority) values (?, ?)";
+    private static final String DEFAULT_ADMIN    = "admin";
+    private static final String ROLE_ADMIN       = "ROLE_ADMIN";
 
     /**
      * We only need to grab the usernames, and check if any exist
@@ -51,15 +49,25 @@ public class ProductionConfig {
             }
         });
 
-        // TODO: Also set ROLE_ADMIN for our user
         if (usernames.isEmpty()) {
             log.info("No users found, registering default admin user");
+
+            // Insert into our users table
             template.update(INSERT_ADMIN, new PreparedStatementSetter() {
                 @Override
                 public void setValues(PreparedStatement preparedStatement) throws SQLException {
                     preparedStatement.setString(1, DEFAULT_ADMIN);
                     preparedStatement.setString(2, DEFAULT_ADMIN);
                     preparedStatement.setBoolean(3, true);
+                }
+            });
+
+            // And into our authorities table
+            template.update(INSERT_AUTHORITY, new PreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                    preparedStatement.setString(1, DEFAULT_ADMIN);
+                    preparedStatement.setString(2, ROLE_ADMIN);
                 }
             });
         }
