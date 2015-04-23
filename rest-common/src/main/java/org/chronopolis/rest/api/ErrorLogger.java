df76ac6f5b32f646766825330fbbc492a13b718a
Michael Ritter <shake@umiacs.umd.edu>
2015-04-23 13:36:01 -0400

    add logger to catch retrofit errors

---
+++ rest-common/src/main/java/org/chronopolis/rest/api/ErrorLogger.java
@@ -0,0 +1,25 @@
+package org.chronopolis.rest.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit.ErrorHandler;
+import retrofit.RetrofitError;
+
+/**
+ * Created by shake on 4/23/15.
+ */
+public class ErrorLogger implements ErrorHandler {
+    private final Logger log = LoggerFactory.getLogger(ErrorLogger.class);
+
+    @Override
+    public Throwable handleError(RetrofitError retrofitError) {
+        log.debug("Handling error from retrofit");
+
+        String url = retrofitError.getUrl();
+        int status = retrofitError.getResponse().getStatus();
+        String reason = retrofitError.getResponse().getReason();
+        log.error("Error in http call: url: {} status: {} reason: {}",new Object[]{url, status, reason});
+
+        return retrofitError.getCause();
+    }
+}
