7c4d09b6ceeafe519c3909be7aa292e7b7cb7984
Michael Ritter <shake@umiacs.umd.edu>
2015-04-30 15:45:12 -0400

    update step listeners to stop execution if necessary, and update the ingest-server if there was a failure

--- replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/BagRESTStepListener.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/BagRESTStepListener.java
@@ -4,6 +4,10 @@ import org.chronopolis.replicate.ReplicationNotifier;
 import org.chronopolis.replicate.config.ReplicationSettings;
 import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.models.Replication;
+import org.chronopolis.rest.models.ReplicationStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
@@ -12,6 +16,7 @@ import org.springframework.batch.core.StepExecutionListener;
  * Created by shake on 12/4/14.
  */
 public class BagRESTStepListener implements StepExecutionListener {
+    private final Logger log = LoggerFactory.getLogger(BagRESTStepListener.class);
 
     private IngestAPI ingestAPI;
     private Replication replication;
@@ -34,10 +39,21 @@ public class BagRESTStepListener implements StepExecutionListener {
 
     @Override
     public ExitStatus afterStep(final StepExecution stepExecution) {
-        String digest = notifier.getCalculatedTagDigest();
-
-        replication.setReceivedTagFixity(digest);
-        ingestAPI.updateReplication(replication.getID(), replication);
+        // Check if we were able to download, if not let the ingest-server know
+        if (notifier.isSuccess()) {
+            String digest = notifier.getCalculatedTagDigest();
+            replication.setReceivedTagFixity(digest);
+            Replication updated = ingestAPI.updateReplication(replication.getID(), replication);
+            if (updated.getStatus() == ReplicationStatus.FAILURE_TAG_MANIFEST) {
+                log.error("Error validating tagmanifest");
+                stepExecution.upgradeStatus(BatchStatus.STOPPED);
+                return ExitStatus.FAILED;
+            }
+        } else {
+            // general failure
+            replication.setStatus(ReplicationStatus.FAILURE);
+            ingestAPI.updateReplication(replication.getID(), replication);
+        }
 
         return ExitStatus.COMPLETED;
     }
--- replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/TokenRESTStepListener.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/TokenRESTStepListener.java
@@ -3,6 +3,10 @@ package org.chronopolis.replicate.batch.listener;
 import org.chronopolis.replicate.ReplicationNotifier;
 import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.models.Replication;
+import org.chronopolis.rest.models.ReplicationStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
@@ -11,6 +15,7 @@ import org.springframework.batch.core.StepExecutionListener;
  * Created by shake on 12/4/14.
  */
 public class TokenRESTStepListener implements StepExecutionListener {
+    private final Logger log = LoggerFactory.getLogger(TokenRESTStepListener.class);
 
     private IngestAPI ingestAPI;
     private Replication replication;
@@ -31,10 +36,25 @@ public class TokenRESTStepListener implements StepExecutionListener {
 
     @Override
     public ExitStatus afterStep(final StepExecution stepExecution) {
-        String digest = notifier.getCalculatedTokenDigest();
+        // Check if we were able to download, if not let the ingest-server know
+        if (notifier.isSuccess()) {
+            log.trace("successful download");
+            String digest = notifier.getCalculatedTokenDigest();
 
-        replication.setReceivedTokenFixity(digest);
-        ingestAPI.updateReplication(replication.getID(), replication);
+            replication.setReceivedTokenFixity(digest);
+            Replication updated = ingestAPI.updateReplication(replication.getID(), replication);
+            if (updated.getStatus() == ReplicationStatus.FAILURE_TOKEN_STORE) {
+                log.error("Error validating token store");
+                // stop the execution
+                stepExecution.upgradeStatus(BatchStatus.STOPPED);
+                return ExitStatus.FAILED;
+            }
+        } else {
+            log.trace("unsuccessful download");
+            // General failure
+            replication.setStatus(ReplicationStatus.FAILURE);
+            ingestAPI.updateReplication(replication.getID(), replication);
+        }
 
         return ExitStatus.COMPLETED;
     }
