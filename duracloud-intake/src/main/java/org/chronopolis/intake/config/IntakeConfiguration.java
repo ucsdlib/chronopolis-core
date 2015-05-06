d66137514aebaf2904a225da6b5b4251c9aa0275
Michael Ritter <shake@umiacs.umd.edu>
2015-05-06 16:20:21 -0400

    add rest adapter for IngestAPI

--- duracloud-intake/src/main/java/org/chronopolis/intake/config/IntakeConfiguration.java
+++ duracloud-intake/src/main/java/org/chronopolis/intake/config/IntakeConfiguration.java
@@ -1,17 +1,17 @@
 package org.chronopolis.intake.config;
 
 import org.chronopolis.amqp.ChronProducer;
+import org.chronopolis.common.ace.CredentialRequestInterceptor;
 import org.chronopolis.common.dpn.DPNService;
 import org.chronopolis.common.mail.MailUtil;
+import org.chronopolis.common.settings.IngestAPISettings;
 import org.chronopolis.common.settings.SMTPSettings;
-import org.chronopolis.db.intake.StatusRepository;
 import org.chronopolis.intake.duracloud.batch.SnapshotJobManager;
-import org.chronopolis.intake.duracloud.batch.SnapshotProcessor;
 import org.chronopolis.intake.duracloud.batch.SnapshotTasklet;
-import org.chronopolis.intake.duracloud.batch.SnapshotWriter;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
-import org.chronopolis.intake.duracloud.model.DuracloudRequest;
 import org.chronopolis.messaging.factory.MessageFactory;
+import org.chronopolis.rest.api.ErrorLogger;
+import org.chronopolis.rest.api.IngestAPI;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit.RestAdapter;
 
 /**
  * Created by shake on 8/4/14.
@@ -33,6 +34,30 @@ public class IntakeConfiguration {
 
     @Autowired
     IntakeSettings intakeSettings;
+
+    @Bean
+    ErrorLogger logger() {
+        return new ErrorLogger();
+    }
+
+    @Bean
+    IngestAPI ingestAPI(IngestAPISettings settings) {
+        String endpoint = settings.getIngestEndpoints().get(0);
+
+        // TODO: This can timeout on long polls, see SO for potential fix
+        // http://stackoverflow.com/questions/24669309/how-to-increase-timeout-for-retrofit-requests-in-robospice-android
+        RestAdapter adapter = new RestAdapter.Builder()
+                .setEndpoint(endpoint)
+                .setErrorHandler(logger())
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setRequestInterceptor(new CredentialRequestInterceptor(
+                        settings.getIngestAPIUsername(),
+                        settings.getIngestAPIPassword()))
+                .build();
+
+        return adapter.create(IngestAPI.class);
+
+    }
 
     @Bean
     MailUtil mailUtil(SMTPSettings smtpSettings) {
@@ -50,42 +75,22 @@ public class IntakeConfiguration {
                                     @Value("#{jobParameters[depositor]}") String depositor,
                                     @Value("#{jobParameters[collectionName]}") String collectionName,
                                     IntakeSettings settings,
-                                    ChronProducer producer,
-                                    MessageFactory messageFactory,
+                                    IngestAPI ingestAPI,
                                     DPNService dpnService) {
         return new SnapshotTasklet(snapshotID,
                 collectionName,
                 depositor,
                 intakeSettings,
-                producer,
-                messageFactory,
+                ingestAPI,
                 dpnService);
     }
 
-    @Bean
-    SnapshotProcessor snapshotProcessor() {
-        return new SnapshotProcessor(intakeSettings);
-    }
-
-    @Bean
-    SnapshotWriter snapshotWriter(ChronProducer producer,
-                                  MessageFactory messageFactory) {
-        return new SnapshotWriter(producer, messageFactory, intakeSettings);
-    }
-
     @Bean(destroyMethod = "destroy")
-    SnapshotJobManager snapshotJobManager(StatusRepository statusRepository,
-                                          SnapshotWriter writer,
-                                          SnapshotProcessor processor,
-                                          JobBuilderFactory jobBuilderFactory,
+    SnapshotJobManager snapshotJobManager(JobBuilderFactory jobBuilderFactory,
                                           StepBuilderFactory stepBuilderFactory,
                                           JobLauncher jobLauncher,
                                           SnapshotTasklet snapshotTasklet) {
-        return new SnapshotJobManager(processor,
-                writer,
-                intakeSettings,
-                statusRepository,
-                jobBuilderFactory,
+        return new SnapshotJobManager(jobBuilderFactory,
                 stepBuilderFactory,
                 jobLauncher,
                 snapshotTasklet);
