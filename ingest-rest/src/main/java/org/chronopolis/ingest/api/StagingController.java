dc37ce73f533abafad33475507966a5f1d1b72d3
Michael Ritter <shake@umiacs.umd.edu>
2015-05-12 16:45:52 -0400

    untar bags if they are serialized upon arrival

--- ingest-rest/src/main/java/org/chronopolis/ingest/api/StagingController.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/StagingController.java
@@ -1,5 +1,7 @@
 package org.chronopolis.ingest.api;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.chronopolis.ingest.IngestSettings;
 import org.chronopolis.ingest.exception.NotFoundException;
 import org.chronopolis.ingest.repository.BagRepository;
@@ -19,6 +21,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -39,7 +45,9 @@ import static org.chronopolis.ingest.api.Params.PAGE_SIZE;
 @RestController
 @RequestMapping("/api")
 public class StagingController {
-    Logger log = LoggerFactory.getLogger(StagingController.class);
+    private static final String TAR_TYPE = "application/x-tar";
+
+    private final Logger log = LoggerFactory.getLogger(StagingController.class);
 
     @Autowired
     BagRepository bagRepository;
@@ -134,6 +142,12 @@ public class StagingController {
         Path stage = Paths.get(ingestSettings.getBagStage());
         Path bagPath = stage.resolve(fileName);
 
+        // check if we should untar the bag
+        String mimeType = Files.probeContentType(bagPath);
+        if (mimeType != null && mimeType.equals(TAR_TYPE)) {
+            bagPath = untar(bagPath, bag.getDepositor());
+        }
+
         // TODO: Get these passed in the ingest request
         final long[] bagSize = {0};
         final long[] fileCount = {0};
@@ -151,6 +165,57 @@ public class StagingController {
         bag.setSize(bagSize[0]);
         bag.setTotalFiles(fileCount[0]);
         bag.setFixityAlgorithm("SHA-256");
+    }
+
+
+    /**
+     * Explode a tarball for a given transfer
+     *  @param tarball
+     * @param depositor
+     */
+    private Path untar(Path tarball, String depositor) throws IOException {
+        String stage = ingestSettings.getBagStage();
+
+        // Set up our tar stream and channel
+        TarArchiveInputStream tais = new TarArchiveInputStream(java.nio.file.Files.newInputStream(tarball));
+        TarArchiveEntry entry = tais.getNextTarEntry();
+        ReadableByteChannel inChannel = Channels.newChannel(tais);
+
+        // Get our root path (just the staging area), and create an updated bag path
+        Path root = Paths.get(stage, depositor);
+        Path bag = root.resolve(entry.getName());
+
+        while (entry != null) {
+            Path entryPath = root.resolve(entry.getName());
+
+            if (entry.isDirectory()) {
+                log.trace("Creating directory {}", entry.getName());
+                java.nio.file.Files.createDirectories(entryPath);
+            } else {
+                log.trace("Creating file {}", entry.getName());
+
+                entryPath.getParent().toFile().mkdirs();
+
+                // In case files are greater than 2^32 bytes, we need to use a
+                // RandomAccessFile and FileChannel to write them
+                RandomAccessFile file = new RandomAccessFile(entryPath.toFile(), "rw");
+                FileChannel out = file.getChannel();
+
+                // The TarArchiveInputStream automatically updates its offset as
+                // it is read, so we don't need to worry about it
+                out.transferFrom(inChannel, 0, entry.getSize());
+                out.close();
+            }
+
+            entry = tais.getNextTarEntry();
+        }
+
+        // basically, get the directory name of the bag
+        //        0       1           2
+        // root = stage + depositor
+        // bag  = stage + depositor + bag-name
+        // root.namecount = 2
+        return root.resolve(bag.getName(root.getNameCount()));
     }
 
 }
