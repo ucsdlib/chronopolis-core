842085c961ce6fb4a5d3c9f49db263c8aabbe2ad
Michael Ritter <shake@umiacs.umd.edu>
2015-05-20 17:21:18 -0400

    add name and depositor params for bag queries

--- ingest-rest/src/main/java/org/chronopolis/ingest/api/Params.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/Params.java
@@ -6,6 +6,8 @@ package org.chronopolis.ingest.api;
  * Created by shake on 1/12/15.
  */
 public class Params {
+    public static final String NAME = "name";
+    public static final String DEPOSITOR = "depositor";
     public static final String PAGE  = "page";
     public static final String PAGE_SIZE = "page_size";
     public static final String STATUS = "status";
