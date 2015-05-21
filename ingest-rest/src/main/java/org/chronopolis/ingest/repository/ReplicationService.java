0082f88624809f3c7eee10ed7d926cee91729f2f
Michael Ritter <shake@umiacs.umd.edu>
2015-05-21 15:08:48 -0400

    add method to find multiple replications; save replications

--- ingest-rest/src/main/java/org/chronopolis/ingest/repository/ReplicationService.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/repository/ReplicationService.java
@@ -2,7 +2,11 @@ package org.chronopolis.ingest.repository;
 
 import com.mysema.query.types.expr.BooleanExpression;
 import org.chronopolis.rest.models.Replication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
@@ -19,6 +23,7 @@ import static org.chronopolis.ingest.repository.PredicateUtil.setExpression;
 @Component
 @Transactional
 public class ReplicationService {
+    private final Logger log = LoggerFactory.getLogger(ReplicationService.class);
 
     @Autowired
     ReplicationRepository replicationRepository;
@@ -40,9 +45,30 @@ public class ReplicationService {
         return replicationRepository.findOne(predicate);
     }
 
+    public Page<Replication> getReplications(ReplicationSearchCriteria criteria, Pageable pageable) {
+        BooleanExpression predicate = null;
+
+        Map<Object, BooleanExpression> criteriaMap = criteria.getCriteria();
+        for (Object o : criteriaMap.keySet()) {
+            setPredicate(o, predicate, criteriaMap.get(o));
+        }
+
+        if (predicate == null) {
+            log.trace("No predicate, returning all replications");
+            return replicationRepository.findAll(pageable);
+        }
+
+        log.trace("Returning replications which satisfy the predicate");
+        return replicationRepository.findAll(predicate, pageable);
+    }
+
+    public void save(Replication replication) {
+        replicationRepository.save(replication);
+    }
+
     public void setPredicate(Object criteria, BooleanExpression predicate, BooleanExpression expression) {
         if (criteria != null) {
-            predicate = setExpression(predicate, expression);
+            setExpression(predicate, expression);
         }
     }
 
