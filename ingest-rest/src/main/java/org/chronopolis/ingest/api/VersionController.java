01e1f39e7a2f56d2b8b62385adbe2f1877b265ac
Michael Ritter <shake@umiacs.umd.edu>
2015-05-29 14:06:28 -0400

    add version controller

---
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/VersionController.java
@@ -0,0 +1,28 @@
+package org.chronopolis.ingest.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shake on 5/29/15.
+ */
+@RestController
+@RequestMapping("/api/version")
+public class VersionController {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Version getVersion() {
+        return new Version();
+    }
+
+    private class Version {
+        private final String version = "1.0-beta";
+
+        public String getVersion() {
+            return version;
+        }
+    }
+
+}
