d74eee5902edf59fda67d9b3dd4f88baddc5c933
Michael Ritter <shake@umiacs.umd.edu>
2015-06-10 16:25:10 -0400

    fix potential npe when serializing a replication as json after a failure in either the bag or token download steps

--- replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/TokenRESTStepListener.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/listener/TokenRESTStepListener.java
@@ -73,6 +73,7 @@ public class TokenRESTStepListener implements StepExecutionListener {
             log.trace("unsuccessful download");
             // General failure
             replication.setStatus(ReplicationStatus.FAILURE);
+            replication.setNodeUser(settings.getNode());
             ingestAPI.updateReplication(replication.getID(), replication);
             sendFailure(mail, settings, replication, stepExecution.getFailureExceptions());
         }
