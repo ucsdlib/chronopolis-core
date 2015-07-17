b3b6f5e3177d9f357c0921d8eb7401ccd5909cae
Michael Ritter <shake@umiacs.umd.edu>
2015-07-17 17:48:47 -0400

    add sql db migration for version 1.1

---
+++ ingest-rest/src/main/java/db/migration/V1_1__add_distribution_table.sql
@@ -0,0 +1,27 @@
+CREATE SEQUENCE bag_distribution_id_seq;
+CREATE TABLE bag_distribution (
+    id bigint PRIMARY KEY DEFAULT nextval('bag_distribution_id_seq'),
+    bag_id bigint,
+    node_id bigint,
+    status varchar(255) -- DEFAULT 'DISTRIBUTE'
+);
+
+ALTER TABLE bag_distribution
+    ADD CONSTRAINT FK_bd_bag FOREIGN KEY (bag_id) REFERENCES bag;
+
+ALTER TABLE bag_distribution
+    ADD CONSTRAINT FK_bd_node FOREIGN KEY (node_id) REFERENCES node;
+
+-- migrate the old data
+INSERT INTO bag_distribution (bag_id, node_id)
+SELECT * FROM bag_replications;
+
+-- drop the constraints/old table
+-- ALTER TABLE bag_replications
+--     DROP CONSTRAINT FK_br_bag;
+
+-- ALTER TABLE bag_replications
+--     DROP CONSTRAINT FK_br_node;
+
+DELETE FROM bag_replications;
+DROP TABLE bag_replications;
