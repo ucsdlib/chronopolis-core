fa31ae25fe0947c40a4330b731445cbd7ae31cbb
Michael Ritter <shake@umiacs.umd.edu>
2015-07-20 16:14:10 -0400

    create basic api for interacting with the bridge

---
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/remote/BridgeAPI.java
@@ -0,0 +1,28 @@
+package org.chronopolis.intake.duracloud.remote;
+
+import org.chronopolis.intake.duracloud.remote.model.Snapshot;
+import org.chronopolis.intake.duracloud.remote.model.SnapshotContent;
+import org.chronopolis.intake.duracloud.remote.model.SnapshotDetails;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+import java.util.List;
+
+/**
+ * API definition for the Brdige server
+ *
+ * Created by shake on 7/20/15.
+ */
+public interface BridgeAPI {
+
+    @GET("/snapshot")
+    List<Snapshot> getSnapshots(@Query("host") String host);
+
+    @GET("/snapshot/{snapshotId}")
+    SnapshotDetails getSnapshotDetails(@Path("snapshotId") String snapshotId);
+
+    @GET("/snapshot/{snapshotId}/content")
+    SnapshotContent getSnapshotContent(@Path("snapshotId") String snapshotId);
+
+}
