ad6ed2174543199e5b2bb26a7c7cc47b458f83e4
Michael Ritter <shake@umiacs.umd.edu>
2015-07-30 17:51:38 -0400

    Add API calls for getting Replications and Nodes from DPN

--- common/src/main/java/org/chronopolis/common/dpn/DPNService.java
+++ common/src/main/java/org/chronopolis/common/dpn/DPNService.java
@@ -1,14 +1,26 @@
 package org.chronopolis.common.dpn;
 
 import retrofit.http.Body;
+import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
+
+import java.util.List;
 
 /**
+ * TODO: Get this from EarthScraper?
+ *
  * Created by shake on 9/30/14.
  */
 public interface DPNService {
 
     @POST("/api-v1/bag/")
     DPNBag createBag(@Body DPNBag bag);
+
+    @GET("/api-v1/node/")
+    List<DPNNode> getNodes();
+
+    @POST("/api-v1/replicate/{id}")
+    DPNReplication createReplication(@Path("id") String id, @Body DPNReplication replication);
 
 }
