56bf2a3b8d865c62f21a5e330a1b66a19ee4daaf
Michael Ritter <shake@umiacs.umd.edu>
2015-08-06 18:00:09 -0400

    Start a test for the ReplicationTask

---
+++ ingest-rest/src/test/java/org/chronopolis/ingest/task/ReplicationTaskTest.java
@@ -0,0 +1,42 @@
+package org.chronopolis.ingest.task;
+
+import org.chronopolis.ingest.IngestTest;
+import org.chronopolis.ingest.TestApplication;
+import org.chronopolis.ingest.repository.ReplicationRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests to validate replications are created properly
+ *
+ * Created by shake on 8/6/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestApplication.class)
+@SqlGroup({
+        // We only want bags to be inserted for these tests
+        // but when tearing down remove the replications as well
+        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/createBags.sql"),
+        @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/deleteReplications.sql")
+})
+public class ReplicationTaskTest extends IngestTest {
+
+    @Autowired
+    ReplicationTask task;
+
+    @Autowired
+    ReplicationRepository repository;
+
+    @Test
+    public void testCreateReplications() throws Exception {
+        task.createReplications();
+
+        repository.findAll();
+    }
+
+}
