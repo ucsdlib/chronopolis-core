466edf6521b052f0034a2f6acb24c27b6dd82d20
Michael Ritter <shake@umiacs.umd.edu>
2015-10-15 15:21:21 -0400

    Add nodeId when creating replication requests

--- rest-common/src/main/java/org/chronopolis/rest/models/ReplicationRequest.java
+++ rest-common/src/main/java/org/chronopolis/rest/models/ReplicationRequest.java
@@ -7,13 +7,23 @@ package org.chronopolis.rest.models;
  */
 public class ReplicationRequest {
 
-    private Long bagID;
+    private Long bagId;
+    private Long nodeId;
 
     public Long getBagId() {
-        return bagID;
+        return bagId;
     }
 
-    public void setBagId(final Long bagID) {
-        this.bagID = bagID;
+    public void setBagId(final Long bagId) {
+        this.bagId = bagId;
+    }
+
+    public Long getNodeId() {
+        return nodeId;
+    }
+
+    public ReplicationRequest setNodeId(Long nodeId) {
+        this.nodeId = nodeId;
+        return this;
     }
 }
