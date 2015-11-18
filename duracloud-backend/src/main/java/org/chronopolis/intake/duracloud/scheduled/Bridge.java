544fe50402f0a8f271b6083c263263888f0e299d
Michael Ritter <shake@umiacs.umd.edu>
2015-11-18 17:08:22 -0500

    Get history when checking snapshots to determine the next step

--- duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/scheduled/Bridge.java
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/scheduled/Bridge.java
@@ -1,10 +1,16 @@
 package org.chronopolis.intake.duracloud.scheduled;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import org.chronopolis.intake.duracloud.batch.SnapshotJobManager;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
+import org.chronopolis.intake.duracloud.model.BagReceipt;
 import org.chronopolis.intake.duracloud.remote.BridgeAPI;
+import org.chronopolis.intake.duracloud.remote.model.HistoryItem;
 import org.chronopolis.intake.duracloud.remote.model.Snapshot;
 import org.chronopolis.intake.duracloud.remote.model.SnapshotDetails;
+import org.chronopolis.intake.duracloud.remote.model.SnapshotHistory;
 import org.chronopolis.intake.duracloud.remote.model.SnapshotStatus;
 import org.chronopolis.intake.duracloud.remote.model.Snapshots;
 import org.slf4j.Logger;
@@ -13,6 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * Define a scheduled task which polls the Bridge server for snapshots
@@ -38,17 +47,39 @@ public class Bridge {
     @Scheduled(cron = "0 * * * * *")
     public void findSnapshots() {
         log.trace("Polling for snapshots...");
-        Snapshots snapshots = bridge.getSnapshots(settings.getDuracloudHost());
+        Snapshots snapshots = bridge.getSnapshots(null, SnapshotStatus.WAITING_FOR_DPN);
         for (Snapshot snapshot : snapshots.getSnapshots()) {
             String snapshotId = snapshot.getSnapshotId();
             if (snapshot.getStatus() == SnapshotStatus.WAITING_FOR_DPN) {
                 log.info("Bagging snapshot ", snapshotId);
                 SnapshotDetails details = bridge.getSnapshotDetails(snapshotId);
+                SnapshotHistory history = bridge.getSnapshotHistory(snapshotId, null);
 
-                // bag and push to chron/dpn
-                manager.startSnapshotTasklet(details);
+                if (history.getTotalCount() > 0) {
+                    // try to deserialize the history returned to us
+                    Gson gson = new GsonBuilder().create();
+                    for (HistoryItem historyItem : history.getHistoryItems()) {
+                        log.info(historyItem.getHistory());
+                        try {
+                            Type type = new TypeToken<List<BagReceipt>>() {}.getType();
+                            List<BagReceipt> bd = gson.fromJson(historyItem.getHistory(), type);
+                            for (BagReceipt receipt : bd) {
+                                log.info("{} ? {} ", receipt.isInitialized(), (receipt.isInitialized() ? receipt.getName() : "null"));
+                                if (receipt.isInitialized()) {
+                                    manager.startReplicationTasklet(details, receipt, settings);
+                                }
+
+                            }
+                        } catch (Exception e) {
+                            log.warn("Error deserializing some of the history", e);
+                        }
+                    }
+                } else {
+                    // bag
+                    manager.startSnapshotTasklet(details);
+                }
+
             }
-
         }
     }
 
