5d7a8760d2ceccb38f72b28b073400e47c566e95
Michael Ritter <shake@umiacs.umd.edu>
2015-11-20 17:08:53 -0500

    Add ReplicationHistory class for the bridge

---
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/model/ReplicationHistorySerializer.java
@@ -0,0 +1,18 @@
+package org.chronopolis.intake.duracloud.model;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+/**
+ *
+ * Created by shake on 11/20/15.
+ */
+public class ReplicationHistorySerializer implements JsonSerializer<ReplicationHistory> {
+    @Override
+    public JsonElement serialize(ReplicationHistory replicationHistory, Type type, JsonSerializationContext jsonSerializationContext) {
+        return null;
+    }
+}
