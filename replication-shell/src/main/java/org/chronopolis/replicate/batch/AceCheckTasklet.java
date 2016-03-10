34bbd29b38a74e27a028c26728f1bacb79206518
Michael Ritter <shake@umiacs.umd.edu>
2016-03-10 15:45:53 -0500

    Add tasklet to check the status of an ACE collection

---
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/AceCheckTasklet.java
@@ -0,0 +1,94 @@
+package org.chronopolis.replicate.batch;
+
+import com.google.common.base.Optional;
+import org.chronopolis.common.ace.AceService;
+import org.chronopolis.common.ace.GsonCollection;
+import org.chronopolis.replicate.batch.callback.UpdateCallback;
+import org.chronopolis.rest.api.IngestAPI;
+import org.chronopolis.rest.models.Bag;
+import org.chronopolis.rest.models.RStatusUpdate;
+import org.chronopolis.rest.models.Replication;
+import org.chronopolis.rest.models.ReplicationStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * fish fish fish fish fish
+ *
+ * Created by shake on 3/10/16.
+ */
+public class AceCheckTasklet implements Tasklet {
+    private final Logger log = LoggerFactory.getLogger(AceCheckTasklet.class);
+
+    private IngestAPI ingest;
+    private AceService aceService;
+    private Replication replication;
+
+    public AceCheckTasklet(IngestAPI ingest, AceService aceService, Replication replication) {
+        this.ingest = ingest;
+        this.aceService = aceService;
+        this.replication = replication;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+        Bag bag = replication.getBag();
+        GetCallback callback = new GetCallback();
+        Call<GsonCollection> call = aceService.getCollectionByName(bag.getName(), bag.getDepositor());
+        call.enqueue(callback);
+        Optional<GsonCollection> collection = callback.get();
+        if (collection.isPresent()) {
+            checkCollection(collection.get());
+        }
+
+        return RepeatStatus.FINISHED;
+    }
+
+    private void checkCollection(GsonCollection gsonCollection) {
+        // TODO: Check for errors as well
+        if (gsonCollection.getState() == 65) {
+            Call<Replication> call = ingest.updateReplicationStatus(replication.getId(), new RStatusUpdate(ReplicationStatus.SUCCESS));
+            call.enqueue(new UpdateCallback());
+        }
+    }
+
+    class GetCallback implements Callback<GsonCollection> {
+
+        GsonCollection collection;
+        CountDownLatch latch = new CountDownLatch(1);
+
+        @Override
+        public void onResponse(Response<GsonCollection> response) {
+            if (response.isSuccess()) {
+                collection = response.body();
+            } else {
+                collection = null;
+            }
+            latch.countDown();
+        }
+
+        @Override
+        public void onFailure(Throwable throwable) {
+            collection = null;
+            latch.countDown();
+        }
+
+        public Optional<GsonCollection> get() {
+            try {
+                latch.await();
+            } catch (InterruptedException ignored) {
+            }
+
+            return Optional.of(collection);
+        }
+    }
+}
