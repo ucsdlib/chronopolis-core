9374e3a511b3ac07d773eed5a0d1f375e0495d29
Michael Ritter <shake@umiacs.umd.edu>
2016-03-10 17:18:03 -0500

    Add helper methods for creating jobs based on our current state

--- replication-shell/src/main/java/org/chronopolis/replicate/batch/ReplicationJobStarter.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/ReplicationJobStarter.java
@@ -4,27 +4,27 @@ import org.chronopolis.common.ace.AceService;
 import org.chronopolis.common.ace.GsonCollection;
 import org.chronopolis.common.mail.MailUtil;
 import org.chronopolis.replicate.ReplicationNotifier;
+import org.chronopolis.replicate.batch.ace.AceTasklet;
 import org.chronopolis.replicate.batch.listener.BagRESTStepListener;
 import org.chronopolis.replicate.batch.listener.TokenRESTStepListener;
 import org.chronopolis.replicate.config.ReplicationSettings;
 import org.chronopolis.rest.api.IngestAPI;
-import org.chronopolis.rest.models.RStatusUpdate;
+import org.chronopolis.rest.models.Bag;
 import org.chronopolis.rest.models.Replication;
-import org.chronopolis.rest.models.ReplicationStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 import java.io.IOException;
@@ -89,28 +89,14 @@ public class ReplicationJobStarter {
         }
 
         if (gsonCollection == null) {
-            ReplicationNotifier notifier = new ReplicationNotifier(replication);
-            TokenRESTStepListener tokenStepListener = new TokenRESTStepListener(mailUtil,
-                    ingestAPI,
-                    replication,
-                    settings,
-                    notifier);
-            BagRESTStepListener bagStepListener = new BagRESTStepListener(mailUtil,
-                    ingestAPI,
-                    replication,
-                    settings,
-                    notifier);
-            TokenDownloadStep tds = new TokenDownloadStep(settings, notifier, replication);
-            BagDownloadStep bds = new BagDownloadStep(settings, notifier, replication);
-            AceRegisterStep ars = new AceRegisterStep(aceService, settings, notifier, replication);
-            ReplicationSuccessStep rss = new ReplicationSuccessStep(mailUtil, settings, notifier);
-
-            createJob(depositor, collection, tds, bds, ars, rss, tokenStepListener, bagStepListener);
-
+            createJob(replication);
         } else {
             // A active
             // N - never completely scanned (default for new collections)
             // E
+
+            createJob(replication);
+            /*
             log.debug("Already have collection, state {}", gsonCollection.getState());
             if (gsonCollection.getState() == 'E') {
                 log.info("Error in collection, replicating again");
@@ -136,64 +122,132 @@ public class ReplicationJobStarter {
 
                 }
             });
+            */
+        }
+    }
+
+    private void createJob(Replication replication) {
+        SimpleJobBuilder builder = null;
+        ReplicationNotifier notifier = new ReplicationNotifier(replication);
+
+        switch (replication.getStatus()) {
+            // Run through the full flow
+            case PENDING:
+            case STARTED:
+                builder = fromPending(replication, notifier);
+                break;
+            // Do all ACE work
+            case TRANSFERRED:
+            // TODO: Separate for registered/loaded
+            case ACE_REGISTERED:
+            case ACE_TOKEN_LOADED:
+                builder = fromTransferred(null, replication, notifier);
+                break;
+            case ACE_AUDITING:
+                builder = fromAceAuditing(replication);
+                break;
+            default:
+                return;
+        }
+
+        Bag bag = replication.getBag();
+        try {
+            jobLauncher.run(builder.build(), new JobParametersBuilder()
+                .addString("depositor", bag.getDepositor())
+                .addString("collection", bag.getName())
+                .addDate("date", new Date())
+                .toJobParameters());
+        } catch (JobExecutionAlreadyRunningException e) {
+            log.error("JobExecutionException", e);
+        } catch (JobRestartException e) {
+            log.error("JobRestartException", e);
+        } catch (JobInstanceAlreadyCompleteException e) {
+            log.error("JobInstanceAlreadyCompletedException", e);
+        } catch (JobParametersInvalidException e) {
+            log.error("JobParametersInvalidException", e);
         }
     }
 
     /**
-     * Build a job and launch it with the given parameters
+     * PENDING,
+     * STARTED,
+     * TRANSFERRED,
+     * SUCCESS,
+     * ACE_REGISTERED,
+     * ACE_TOKEN_LOADED,
+     * ACE_AUDITING,
      *
-     * @param depositor
-     * @param collection
-     * @param tokenDownloadStep
-     * @param bagDownloadStep
-     * @param aceRegisterStep
-     * @param replicationSuccessStep
-     * @param tokenStepListener
-     * @param bagStepListener
      */
-    private void createJob(String depositor,
-                           String collection,
-                           TokenDownloadStep tokenDownloadStep,
-                           BagDownloadStep bagDownloadStep,
-                           AceRegisterStep aceRegisterStep,
-                           ReplicationSuccessStep replicationSuccessStep,
-                           StepExecutionListener tokenStepListener,
-                           StepExecutionListener bagStepListener) {
-        Job job = jobBuilderFactory.get("collection-replicate")
-                .start(stepBuilderFactory.get("token-replicate")
+
+    /**
+     * Create a JobBuilder with all steps needed from the PENDING state
+     *
+     * @param replication
+     * @param notifier
+     * @return
+     */
+    private SimpleJobBuilder fromPending(Replication replication, ReplicationNotifier notifier) {
+        TokenDownloadStep tokenDownloadStep = new TokenDownloadStep(settings, notifier, replication);
+        TokenRESTStepListener tokenStepListener = new TokenRESTStepListener(mailUtil, ingestAPI, replication, settings, notifier);
+
+        BagDownloadStep bagDownloadStep = new BagDownloadStep(settings, notifier, replication);
+        BagRESTStepListener bagStepListener = new BagRESTStepListener(mailUtil, ingestAPI, replication, settings, notifier);
+
+        JobBuilder builder = jobBuilderFactory.get("collection-replicate");
+        SimpleJobBuilder start = builder.start(stepBuilderFactory.get("token-replicate")
                         .tasklet(tokenDownloadStep)
                         .listener(tokenStepListener)
                         .build())
                 .next(stepBuilderFactory.get("bag-replicate")
                         .tasklet(bagDownloadStep)
                         .listener(bagStepListener)
-                        .build())
-                .next(stepBuilderFactory.get("ace-register")
-                        .tasklet(aceRegisterStep)
-                        .listener(replicationStepListener)
-                        .build())
-                .next(stepBuilderFactory.get("replication-success")
-                        .tasklet(replicationSuccessStep)
-                        .listener(replicationStepListener)
-                        .build())
+                        .build());
+        return fromTransferred(start, replication, notifier);
+    }
+
+    /**
+     * Create a JobBuilder with all steps needed after a transfer has completed
+     *
+     * @param builder
+     * @param replication
+     * @param notifier
+     * @return
+     */
+    private SimpleJobBuilder fromTransferred(SimpleJobBuilder builder, Replication replication, ReplicationNotifier notifier) {
+        AceTasklet at = new AceTasklet(ingestAPI, aceService, replication, settings, notifier);
+        TaskletStep step = stepBuilderFactory.get("ace-task")
+                .tasklet(at)
+                .listener(replicationStepListener)
                 .build();
 
-        try {
-            jobLauncher.run(job, new JobParametersBuilder()
-                    .addString("depositor", depositor)
-                    .addString("collection", collection)
-                    .addDate("date", new Date())
-                    .toJobParameters());
-        } catch (JobExecutionAlreadyRunningException e) {
-            log.error("JobExecutionException", e);
-        } catch (JobRestartException e) {
-            log.error("JobRestartException", e);
-        } catch (JobInstanceAlreadyCompleteException e) {
-            log.error("JobAlreadyCompletedException", e);
-        } catch (JobParametersInvalidException e) {
-            log.error("JobInvalidParamsException", e);
+        if (builder == null) {
+            JobBuilder jb = jobBuilderFactory.get("collection-replicate");
+            return jb.start(step);
+        } else {
+            return builder.next(step);
         }
+    }
 
+    private SimpleJobBuilder fromAceRegistered(SimpleJobBuilder builder, Replication replication) {
+        return null;
+    }
+
+    private SimpleJobBuilder fromAceTokenLoaded(SimpleJobBuilder builder, Replication replication) {
+        return null;
+    }
+
+    /**
+     * Create a JobBuilder for checking the status of ACE audits
+     *
+     * @param replication
+     * @return
+     */
+    private SimpleJobBuilder fromAceAuditing(Replication replication) {
+        AceCheckTasklet tasklet = new AceCheckTasklet(ingestAPI, aceService, replication);
+        JobBuilder builder = jobBuilderFactory.get("ace-check");
+        return builder.start(stepBuilderFactory.get("ace-check")
+                .tasklet(tasklet)
+                .build());
     }
 
 }
