dac9dec118523962d3b320a4190122f60c54e8c8
Michael Ritter <shake@umiacs.umd.edu>
2016-06-02 14:39:38 -0400

    Add test for chronopolis ingest task

---
+++ duracloud-backend/src/test/java/org/chronopolis/intake/duracloud/batch/ChronopolisIngestTest.java
@@ -0,0 +1,42 @@
+package org.chronopolis.intake.duracloud.batch;
+
+import org.chronopolis.rest.api.IngestAPI;
+import org.chronopolis.rest.entities.Bag;
+import org.chronopolis.rest.models.IngestRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * Created by shake on 6/2/16.
+ */
+@SuppressWarnings("ALL")
+public class ChronopolisIngestTest extends BatchTestBase {
+
+    @Mock IngestAPI api;
+
+    ChronopolisIngest ingest;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        ingest = new ChronopolisIngest(data(), receipts(), api, settings);
+    }
+
+    @Test
+    public void run() throws Exception {
+        when(api.stageBag(any(IngestRequest.class))).thenReturn(new CallWrapper<Bag>(createChronBag()));
+        ingest.run();
+
+        verify(api, times(2)).stageBag(any(IngestRequest.class));
+    }
+
+}
