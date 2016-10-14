95e119ab33663316b6586a710cf5e6a3a10ee18f
Michael Ritter <shake@umiacs.umd.edu>
2016-10-14 16:26:53 -0400

    Copy from AceTasklet

--- replication-shell/src/main/java/org/chronopolis/replicate/batch/ace/AceRunner.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/ace/AceRunner.java
@@ -1,5 +1,14 @@
 package org.chronopolis.replicate.batch.ace;
 
+import com.google.common.collect.ImmutableList;
+import org.chronopolis.common.ace.AceService;
+import org.chronopolis.replicate.ReplicationNotifier;
+import org.chronopolis.replicate.config.ReplicationSettings;
+import org.chronopolis.rest.api.IngestAPI;
+import org.chronopolis.rest.entities.Replication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Manage the 3 ACE steps we do
  * 1 - ACE_REGISTER
@@ -12,8 +21,38 @@ package org.chronopolis.replicate.batch.ace;
  * Created by shake on 10/13/16.
  */
 public class AceRunner implements Runnable {
+    private final Logger log = LoggerFactory.getLogger(AceRunner.class);
+
+    AceService ace;
+    IngestAPI ingest;
+    Replication replication;
+    ReplicationSettings settings;
+
+    // TODO: May be able to remove this
+    ReplicationNotifier notifier;
+
     @Override
     public void run() {
+        AceRegisterTasklet register = new AceRegisterTasklet(ingest, ace, replication, settings, notifier);
+        Long id = null;
+        try {
+            id = register.call();
+            rest(id);
+        } catch (Exception e) {
+            log.error("Error communicating with ACE", e);
+        }
+    }
 
+    private void rest(Long id) {
+        // TODO: We will probably want to break this up more - and do some validation along the way
+        //       - load tokens + validate we have the expected amount (maybe pull info from ingest)
+        //       - run audit
+        AceTokenTasklet token = new AceTokenTasklet(ingest, ace, replication, settings, notifier, id);
+        AceAuditTasklet audit = new AceAuditTasklet(ingest, ace, replication, settings, notifier, id);
+        for (Runnable runnable : ImmutableList.of(token, audit)) {
+            if (notifier.isSuccess()) {
+                runnable.run();
+            }
+        }
     }
 }
