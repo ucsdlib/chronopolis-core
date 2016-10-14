57add4995bb0f0a5176a86af964f85f5a0b5429a
Michael Ritter <shake@umiacs.umd.edu>
2016-10-14 13:51:21 -0400

    Stub Runnables for ace tasks

---
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/AceCheck.java
@@ -0,0 +1,14 @@
+package org.chronopolis.replicate.batch;
+
+/**
+ * fish fish fish fish fish
+ * eating fish
+ *
+ * Created by shake on 10/13/16.
+ */
+public class AceCheck implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ replication-shell/src/main/java/org/chronopolis/replicate/batch/ace/AceRunner.java
@@ -0,0 +1,19 @@
+package org.chronopolis.replicate.batch.ace;
+
+/**
+ * Manage the 3 ACE steps we do
+ * 1 - ACE_REGISTER
+ * 2 - ACE_LOAD
+ * 3 - ACE_AUDIT
+ *
+ * TODO: We may want validation between some of these steps so we know they
+ *       completed successfully.
+ *
+ * Created by shake on 10/13/16.
+ */
+public class AceRunner implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
