375b4a2116b255c491a8dc023a28505b3eb328d5
Michael Ritter <shake@umiacs.umd.edu>
2016-10-18 16:40:39 -0400

    Update querying to get the values from the ReplicationStatus enum instead of hard coding

--- replication-shell/src/main/java/org/chronopolis/replicate/scheduled/ReplicationQueryTask.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/scheduled/ReplicationQueryTask.java
@@ -1,7 +1,6 @@
 package org.chronopolis.replicate.scheduled;
 
 import org.chronopolis.common.settings.IngestAPISettings;
-import org.chronopolis.replicate.batch.ReplicationJobStarter;
 import org.chronopolis.replicate.batch.Submitter;
 import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.entities.Replication;
@@ -19,14 +18,12 @@ import org.springframework.stereotype.Component;
 import retrofit2.Call;
 import retrofit2.Response;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import static org.chronopolis.rest.models.ReplicationStatus.STARTED;
 
 /**
  * Scheduled task for checking the ingest-server for replication requests
@@ -51,71 +48,37 @@ public class ReplicationQueryTask {
 
     /**
      * Check the ingest-server for pending and started replications
-     *
-     * TODO: We now have a host of states (ACE_*) which we will want to check
-     *
+     * <p>
      */
     @Scheduled(cron = "${replication.cron:0 0 * * * *}")
     public void checkForReplications() {
-        //Set<String> filter = activeReplications();
-        Set<String> filter = new HashSet<>();
+        Arrays.stream(ReplicationStatus.values())
+                .filter(ReplicationStatus::isOngoing) // make sure our replication is part of the valid flow
+                .map(this::query)                     // run the query method
+                .anyMatch(q -> {                      // short circuit in case we have an exception
+                    if (!q.success) {
+                        log.error("Error checking for replications", q.t);
+                        return true;
+                    }
 
-        try {
-            log.info("Query for active replications");
-            query(STARTED, filter, false);
-
-            log.info("Query for new replications");
-            query(ReplicationStatus.PENDING, filter, true);
-
-            log.info("Query for transferred replications");
-            query(ReplicationStatus.TRANSFERRED, filter, false);
-
-            log.info("Query for ace flow replications");
-            query(ReplicationStatus.ACE_REGISTERED, filter, false);
-            query(ReplicationStatus.ACE_TOKEN_LOADED, filter, false);
-
-            log.info("Query for auditing replications");
-            query(ReplicationStatus.ACE_AUDITING, filter, false);
-        } catch (IOException e) {
-            log.error("Error checking for replications", e);
-        }
-
+                    return false;
+                });
     }
-
-    /*
-     * Create a set of ongoing replications, specified by
-     * depositor:collection
-     *
-     * @return Set of active replications
-    private Set<String> activeReplications() {
-        Set<String> filter = new HashSet<>();
-        Set<JobExecution> executions = explorer.findRunningJobExecutions("collection-replicate");
-        for (JobExecution execution : executions) {
-            JobParameters params = execution.getJobParameters();
-            String depositor = params.getString("depositor");
-            String collection = params.getString("collection");
-
-            filter.add(depositor + ":" + collection);
-        }
-
-        return filter;
-    }
-     */
 
     /**
-     * Query the ingest-server and add requests to the {@link ReplicationJobStarter}
+     * Query the ingest-server and add requests to the {@link Submitter}
      * if they are not already being replicated
      *
      * @param status - the status of the request to get
-     * @param filter - the Set of active replications to filter on
-     * @param update - whether or not to update the stats (to STARTED)
      */
-    private void query(ReplicationStatus status, Set<String> filter, boolean update) throws IOException {
+    private Query query(ReplicationStatus status) {
+        log.info("Querying for {} replications", status.toString());
+
         int page = 0;
         int pageSize = 20;
         boolean hasNext = true;
 
-
+        Query q = new Query(true);
         Map<String, Object> params = new HashMap<>();
         params.put("page", page);
         params.put("status", status);
@@ -125,71 +88,55 @@ public class ReplicationQueryTask {
         // TODO: As replications get updated, the state can change and alter the
         // amount of pages. We might want to switch this to only work on one page
         // at a time or figure something else out.
-        while (hasNext) {
-            Call<PageImpl<Replication>> call = ingestAPI.getReplications(params);
-            Response<PageImpl<Replication>> response = call.execute();
-            Page<Replication> replications = response.body();
-            log.debug("[{}] On page {} with {} replications. {} total.", new Object[]{
-                    status,
-                    replications.getNumber(),
-                    replications.getNumberOfElements(),
-                    replications.getTotalElements()});
+        try {
+            while (hasNext) {
+                Call<PageImpl<Replication>> call = ingestAPI.getReplications(params);
+                Response<PageImpl<Replication>> response = call.execute();
+                Page<Replication> replications = response.body();
+                log.debug("[{}] On page {} with {} replications. {} total.", new Object[]{
+                        status,
+                        replications.getNumber(),
+                        replications.getNumberOfElements(),
+                        replications.getTotalElements()});
 
-            ++page;
-            hasNext = replications.hasNext();
-            params.put("page", page);
+                ++page;
+                hasNext = replications.hasNext();
+                params.put("page", page);
 
-            startReplications(replications.getContent(), filter, update);
+                startReplications(replications.getContent());
+            }
+        } catch (IOException e) {
+            q = new Query(false, e);
         }
+
+        return q;
     }
 
-    private void startReplications(List<Replication> replications, Set<String> filter, boolean update) throws IOException {
+    private void startReplications(List<Replication> replications) throws IOException {
         for (Replication replication : replications) {
             log.debug("Replication {} has bag-id {}", replication.getId(), replication.getBagId());
+            // TODO: It would be nice if the bag was part of the replication. Need to update the API.
             Call<Bag> call = ingestAPI.getBag(replication.getBagId());
             Response<Bag> response = call.execute();
             Bag bag = response.body();
-
             replication.setBag(BagConverter.toBagEntity(bag));
 
-            // log.info("Submitter is null? {}", submitter == null);
             submitter.submit(replication);
+        }
+    }
 
-            /*
-            String filterString = bag.getDepositor() + ":" + bag.getName();
-            if (update) {
-                log.info("Updating replication");
+    private class Query {
+        boolean success;
+        @Nullable
+        Throwable t;
 
-                // The anonymous classes are temporary for now, while the calls are updated to the new methods
-                Call<Replication> statusCall = ingestAPI.updateReplicationStatus(replication.getId(), new RStatusUpdate(STARTED));
-                final long id = replication.getId();
-                statusCall.enqueue(new Callback<Replication>() {
-                    @Override
-                    public void onResponse(Response<Replication> response) {
-                        log.debug("Update to replication {}: {} - {}", new Object[]{id,
-                                response.code(),
-                                response.message()});
-                    }
+        public Query(boolean success) {
+            this.success = success;
+        }
 
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        log.error("Error communicating to the ingest server", throwable);
-                    }
-                });
-            }
-
-            // Make sure we don't have a replication already in progress
-            if (!filter.contains(filterString)) {
-                log.info("Starting job for replication id {}", replication.getId());
-                // jobStarter.addJobFromRestful(replication);
-                submitter.submit(replication);
-
-                // Add our current execution to our filter list
-                filter.add(filterString);
-            } else {
-                log.info("Skipping replication {}, already in progress", replication.getId());
-            }
-            */
+        public Query(boolean success, Throwable t) {
+            this.success = success;
+            this.t = t;
         }
     }
 
