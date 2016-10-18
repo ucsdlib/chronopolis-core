4c7d83c93d2ee409711ca13b037971f07e614f82
Michael Ritter <shake@umiacs.umd.edu>
2016-10-18 16:37:57 -0400

    Remove spring-batch config

--- replication-shell/src/main/java/org/chronopolis/replicate/config/ReplicationConfig.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/config/ReplicationConfig.java
@@ -25,9 +25,6 @@ import org.chronopolis.rest.support.ZonedDateTimeDeserializer;
 import org.chronopolis.rest.support.ZonedDateTimeSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -49,12 +46,8 @@ import java.util.concurrent.TimeUnit;
  */
 @SuppressWarnings("ALL")
 @Configuration
-@EnableBatchProcessing
 public class ReplicationConfig {
     public final Logger log = LoggerFactory.getLogger(ReplicationConfig.class);
-
-    @Autowired
-    JobRepository jobRepository;
 
     @Value("${debug.retrofit:NONE}")
     String retrofitLogLevel;
@@ -181,51 +174,6 @@ public class ReplicationConfig {
         return new TrackingThreadPoolExecutor<>(2, 2, 30, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
     }
 
-    /*
-     * Class to handle creation of replication jobs through spring-batch
-     *
-     * @param settings
-     * @param mailUtil
-     * @param aceService
-     * @param ingestAPI
-     * @param replicationStepListener
-     * @param jobLauncher
-     * @param jobBuilderFactory
-     * @param stepBuilderFactory
-     * @return
-    @Bean
-    ReplicationJobStarter jobStarter(ReplicationSettings settings,
-                                     MailUtil mailUtil,
-                                     AceService aceService,
-                                     IngestAPI ingestAPI,
-                                     ReplicationStepListener replicationStepListener,
-                                     JobLauncher jobLauncher,
-                                     JobBuilderFactory jobBuilderFactory,
-                                     StepBuilderFactory stepBuilderFactory) {
-        return new ReplicationJobStarter(settings,
-                mailUtil,
-                aceService,
-                ingestAPI,
-                replicationStepListener,
-                jobLauncher,
-                jobBuilderFactory,
-                stepBuilderFactory);
-    }
-     */
-
-    /*
-     * Step listener for the {@link ReplicationJobStarter}
-     *
-     * @param replicationSettings
-     * @param mailUtil
-     * @return
-    @Bean
-    ReplicationStepListener replicationStepListener(ReplicationSettings replicationSettings,
-                                                    MailUtil mailUtil) {
-        return new ReplicationStepListener(replicationSettings, mailUtil);
-    }
-     */
-
     /**
      * Class to send email notifications regarding replications
      *
@@ -241,34 +189,5 @@ public class ReplicationConfig {
         mailUtil.setSmtpSend(smtpSettings.getSend());
         return mailUtil;
     }
-
-    /*
-     * Async task executor so we can have multiple threads execute at once
-     * for spring-batch
-     *
-     * @return
-    @Bean
-    TaskExecutor simpleAsyncTaskExecutor() {
-        SimpleAsyncTaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
-        taskExecutor.setConcurrencyLimit(2);
-        return taskExecutor;
-    }
-     */
-
-
-    /*
-     * SimpleJobLauncher so that we use the {@link TaskExecutor}
-     *
-     * @param jobRepository
-     * @param simpleAsyncTaskExecutor
-     * @return
-    @Bean
-    JobLauncher jobLauncher(JobRepository jobRepository, TaskExecutor simpleAsyncTaskExecutor) {
-        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-        jobLauncher.setJobRepository(jobRepository);
-        jobLauncher.setTaskExecutor(simpleAsyncTaskExecutor);
-        return jobLauncher;
-    }
-     */
 
 }
