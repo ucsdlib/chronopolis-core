0a3dc49bd044819ccabca2e333d9511c9728340b
Michael Ritter <shake@umiacs.umd.edu>
2016-10-27 17:31:12 -0400

    Add cleanup for chron

--- duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/scheduled/Cleaner.java
+++ duracloud-backend/src/main/java/org/chronopolis/intake/duracloud/scheduled/Cleaner.java
@@ -7,9 +7,11 @@ import org.chronopolis.earth.models.Bag;
 import org.chronopolis.intake.duracloud.config.IntakeSettings;
 import org.chronopolis.intake.duracloud.remote.BridgeAPI;
 import org.chronopolis.rest.api.IngestAPI;
+import org.chronopolis.rest.models.BagStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -65,7 +67,6 @@ public class Cleaner {
         Path parent = tarchive.getParent().getFileName();
 
         String uuid = name.toString().substring(0, name.toString().lastIndexOf(".tar"));
-
         log.info("Checking {}/{} replication status", parent, uuid);
         // TODO: Get snapshot based on alt id (no support in bridge yet)
         // TODO: Could use enqueue instead of execute
@@ -85,12 +86,51 @@ public class Cleaner {
     }
 
 
-    public void cleanChron(Path bag) {
-        Path name = bag.getFileName();
-        Path parent = bag.getParent().getFileName();
+    @Scheduled(cron = "0 0 0 * * *")
+    public void cleanChron() {
+        String bagStage = settings.getBagStage();
+        Path bags = Paths.get(bagStage);
 
-        ingest.getBags(ImmutableMap.of(
+        try {
+            Files.walk(bags)
+                    .filter(p -> (p.getNameCount() - bags.getNameCount()) == 2)
+                    .filter(p -> Files.isDirectory(p))
+                    .forEach(this::chron);
+        } catch (IOException e) {
+            log.warn("Error walking bag stage", e);
+        }
+
+    }
+
+    private void chron(Path path) {
+        Path name = path.getFileName();
+        Path parent = path.getParent().getFileName();
+
+        log.info("Checking {}/{} replication status", parent, name);
+
+        Call<PageImpl<org.chronopolis.rest.models.Bag>> bags = ingest.getBags(ImmutableMap.of(
                 "name", name.toString(),
                 "depositor", parent.toString()));
+        try {
+            Response<PageImpl<org.chronopolis.rest.models.Bag>> response = bags.execute();
+            if (response.isSuccessful()) {
+                PageImpl<org.chronopolis.rest.models.Bag> body = response.body();
+                if (body.getTotalElements() == 0 || body.getTotalElements() > 1) {
+                    log.warn("Multiple bags found, aborting");
+                } else {
+                    org.chronopolis.rest.models.Bag bag = body.getContent().get(0);
+                    if (bag.getStatus() == BagStatus.PRESERVED) {
+                        log.info("REMOVING {} at {}", name, path);
+                        Files.delete(path);
+                    }
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            log.warn("Error communicating with ingest", e);
+        }
     }
+
 }
