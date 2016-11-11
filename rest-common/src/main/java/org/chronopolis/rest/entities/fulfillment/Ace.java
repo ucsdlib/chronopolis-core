aab1239b7b76199fd365c420b19d33d904df4fa4
Michael Ritter <shake@umiacs.umd.edu>
2016-11-11 13:48:29 -0500

    Add STI model for our transfer strategy

---
+++ rest-common/src/main/java/org/chronopolis/rest/entities/fulfillment/Ace.java
@@ -0,0 +1,37 @@
+package org.chronopolis.rest.entities.fulfillment;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+/**
+ *
+ * Created by shake on 11/11/16.
+ */
+@Entity
+@DiscriminatorValue(value = "ACE")
+public class Ace extends Strategy {
+
+    String apiKey;
+    String url;
+
+    public Ace() {
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public Ace setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Ace setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+}
---
+++ rest-common/src/main/java/org/chronopolis/rest/entities/fulfillment/Strategy.java
@@ -0,0 +1,46 @@
+package org.chronopolis.rest.entities.fulfillment;
+
+import org.chronopolis.rest.models.repair.Fulfillment;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Abstract class representing a fulfillment strategy
+ *
+ * Created by shake on 11/11/16.
+ */
+@Entity
+@Inheritance
+@DiscriminatorColumn(name = "TYPE")
+@Table(name = "strategy")
+public abstract class Strategy {
+
+    @Id
+    private Long id;
+
+    @OneToOne
+    private Fulfillment fulfillment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Strategy setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Fulfillment getFulfillment() {
+        return fulfillment;
+    }
+
+    public Strategy setFulfillment(Fulfillment fulfillment) {
+        this.fulfillment = fulfillment;
+        return this;
+    }
+}
