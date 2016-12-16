55fd49bba37a2ebdddc73dcb79c5a92428c6b4d1
Michael Ritter <shake@umiacs.umd.edu>
2016-12-16 17:41:02 -0500

    Create serializers  to return models instead of entities from the rest api

---
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/serializer/BagSerializer.java
@@ -0,0 +1,22 @@
+package org.chronopolis.ingest.api.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.chronopolis.rest.entities.Bag;
+import org.chronopolis.rest.support.BagConverter;
+
+import java.io.IOException;
+
+/**
+ * Jackson serializer for Bag entity -> Bag model
+ *
+ * Created by shake on 12/16/16.
+ */
+public class BagSerializer extends JsonSerializer<Bag> {
+    @Override
+    public void serialize(Bag bag, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeObject(BagConverter.toBagModel(bag));
+    }
+}
---
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/serializer/ZonedDateTimeDeserializer.java
@@ -0,0 +1,24 @@
+package org.chronopolis.ingest.api.serializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Jackson deserializer for ZonedDateTime objects
+ *
+ * Created by shake on 12/16/16.
+ */
+public class ZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
+    @Override
+    public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        DateTimeFormatter fmt = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneOffset.UTC);
+        String text = jsonParser.getText();
+        return ZonedDateTime.from(fmt.parse(text));
+    }
+}
