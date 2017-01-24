319bce55eca78809888623b173092d0bf82bc158
Michael Ritter <shake@umiacs.umd.edu>
2017-01-24 17:09:06 -0500

    Repair repo, search criteria, and service

---
+++ ingest-rest/src/main/java/org/chronopolis/ingest/repository/RepairSearchCriteria.java
@@ -0,0 +1,45 @@
+package org.chronopolis.ingest.repository;
+
+import com.mysema.query.types.expr.BooleanExpression;
+import org.chronopolis.ingest.api.Params;
+import org.chronopolis.rest.entities.QRepair;
+import org.chronopolis.rest.models.repair.RepairStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * Created by shake on 1/24/17.
+ */
+public class RepairSearchCriteria implements SearchCriteria {
+    private final QRepair repair;
+
+    private Map<Object, BooleanExpression> criteria;
+
+    public RepairSearchCriteria() {
+        repair = QRepair.repair;
+        criteria = new HashMap<>();
+    }
+
+    public RepairSearchCriteria withStatus(RepairStatus status) {
+        if (status != null) {
+            criteria.put(Params.STATUS, repair.status.eq(status));
+        }
+        return this;
+    }
+
+    public RepairSearchCriteria withRequester(String requester) {
+        if (requester != null && requester.isEmpty()) {
+            criteria.put(Params.REQUESTER, repair.requester.eq(requester));
+        }
+
+        return this;
+    }
+
+
+    @Override
+    public Map<Object, BooleanExpression> getCriteria() {
+        return criteria;
+    }
+}
