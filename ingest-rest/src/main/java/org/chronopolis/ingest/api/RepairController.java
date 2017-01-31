c37f9d1ad8a096e1d0339cfdcca58fb095b160f5
Michael Ritter <shake@umiacs.umd.edu>
2017-01-31 15:45:23 -0500

    Initial impl of fulfillment api

--- ingest-rest/src/main/java/org/chronopolis/ingest/api/RepairController.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/api/RepairController.java
@@ -1,31 +1,43 @@
 package org.chronopolis.ingest.api;
 
+import com.google.common.collect.ImmutableMap;
 import org.chronopolis.ingest.exception.BadRequestException;
 import org.chronopolis.ingest.exception.UnauthorizedException;
 import org.chronopolis.ingest.repository.BagSearchCriteria;
 import org.chronopolis.ingest.repository.BagService;
+import org.chronopolis.ingest.repository.FulfillmentRepository;
+import org.chronopolis.ingest.repository.FulfillmentSearchCriteria;
 import org.chronopolis.ingest.repository.NodeRepository;
 import org.chronopolis.ingest.repository.RepairRepository;
 import org.chronopolis.ingest.repository.RepairSearchCriteria;
 import org.chronopolis.ingest.repository.RepairService;
+import org.chronopolis.ingest.repository.SearchService;
 import org.chronopolis.rest.entities.Bag;
+import org.chronopolis.rest.entities.Fulfillment;
 import org.chronopolis.rest.entities.Node;
 import org.chronopolis.rest.entities.Repair;
 import org.chronopolis.rest.entities.RepairFile;
+import org.chronopolis.rest.entities.fulfillment.Strategy;
+import org.chronopolis.rest.models.repair.FulfillmentStatus;
+import org.chronopolis.rest.models.repair.FulfillmentStrategy;
 import org.chronopolis.rest.models.repair.RepairRequest;
 import org.chronopolis.rest.models.repair.RepairStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Map;
 
+import static org.chronopolis.ingest.IngestController.createPageRequest;
 import static org.chronopolis.ingest.IngestController.hasRoleAdmin;
 
 /**
@@ -40,15 +52,17 @@ public class RepairController {
     private final BagService bService;
     private final NodeRepository nodes;
     private final RepairService<Repair, Long, RepairRepository> rService;
-    // SearchService<Fulfillment, Long, FulfillmentRepository> fService;
+    private final SearchService<Fulfillment, Long, FulfillmentRepository> fService;
 
     @Autowired
     public RepairController(BagService bService,
                             NodeRepository nodes,
-                            RepairService<Repair, Long, RepairRepository> rService) {
+                            RepairService<Repair, Long, RepairRepository> rService,
+                            SearchService<Fulfillment, Long, FulfillmentRepository> fService) {
         this.bService = bService;
         this.nodes = nodes;
         this.rService = rService;
+        this.fService = fService;
     }
 
     @RequestMapping(value = "/requests", method = RequestMethod.GET)
@@ -111,7 +125,66 @@ public class RepairController {
     }
 
     @RequestMapping(value = "/requests/{id}/fulfill", method = RequestMethod.POST)
-    public void fulfillRequest() {
+    public Fulfillment fulfillRequest(Principal principal, @PathVariable("id") Long id) {
+        // todo: contraints (node exists, repair exists)
+        // todo: create lock for creation of fulfillment on a repair?
+        Node n = nodes.findByUsername(principal.getName());
+        RepairSearchCriteria criteria = new RepairSearchCriteria()
+                .withId(id);
+        Repair repair = rService.find(criteria);
+        repair.setStatus(RepairStatus.FULFILLING);
+
+        Fulfillment f = new Fulfillment();
+        f.setRepair(repair)
+         .setStatus(FulfillmentStatus.STAGING)
+         .setFrom(n);
+        repair.setFulfillment(f);
+
+        log.info("Fulfilling request for {}", repair.getTo().getUsername());
+        fService.save(f);
+        return f;
+    }
+
+    @RequestMapping(value = "/fulfillments", method = RequestMethod.GET)
+    public Page<Fulfillment> getFulfillments(@RequestParam Map<String, String> params) {
+        FulfillmentSearchCriteria criteria = new FulfillmentSearchCriteria();
+        return fService.findAll(criteria, createPageRequest(params, ImmutableMap.of()));
+    }
+
+    @RequestMapping(value = "/fulfillments/{id}", method = RequestMethod.GET)
+    public Fulfillment getFulfillment(@PathVariable("id") Long id) {
+        FulfillmentSearchCriteria criteria = new FulfillmentSearchCriteria()
+                .withId(id);
+        return fService.find(criteria);
+    }
+
+    @RequestMapping(value = "/fulfillments/{id}/ready", method = RequestMethod.PUT)
+    public Fulfillment readyFulfillment(Principal principal, @RequestBody FulfillmentStrategy strategy, @PathVariable("id") Long id) {
+        // todo: constraints
+        FulfillmentSearchCriteria criteria = new FulfillmentSearchCriteria()
+                .withId(id);
+        Fulfillment fulfillment = fService.find(criteria);
+
+        Strategy entity = strategy.createEntity();
+        fulfillment.setStrategy(entity);
+        fulfillment.setType(strategy.getType());
+        fulfillment.setStatus(FulfillmentStatus.READY);
+        fService.save(fulfillment);
+        return fulfillment;
+    }
+
+    @RequestMapping(value = "/fulfillments/{id}/complete", method = RequestMethod.PUT)
+    public Fulfillment completeFulfillment(Principal principal, @PathVariable("id") Long id) {
+        FulfillmentSearchCriteria criteria = new FulfillmentSearchCriteria()
+                .withId(id);
+        Fulfillment fulfillment = fService.find(criteria);
+        Repair repair = fulfillment.getRepair();
+
+        fulfillment.setStatus(FulfillmentStatus.COMPLETE);
+        repair.setStatus(RepairStatus.REPAIRED);
+        fService.save(fulfillment);
+        rService.save(repair);
+        return fulfillment;
     }
 
     /**
