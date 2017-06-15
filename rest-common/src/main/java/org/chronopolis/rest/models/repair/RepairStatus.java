4da115d5553d596f84c5c6c4aaa95b75579f42fa
Michael Ritter <shake@umiacs.umd.edu>
2017-06-15 15:42:47 -0400

    Static helpers to group common enums

--- rest-common/src/main/java/org/chronopolis/rest/models/repair/RepairStatus.java
+++ rest-common/src/main/java/org/chronopolis/rest/models/repair/RepairStatus.java
@@ -1,10 +1,23 @@
 package org.chronopolis.rest.models.repair;
 
+import com.google.common.collect.ImmutableListMultimap;
+
 /**
  * Enumeration of the various statuses our repairs can have
  *
  * Created by shake on 11/10/16.
  */
 public enum RepairStatus {
-    REQUESTED, STAGING, READY, TRANSFERRED, REPAIRED, FAILED
+    REQUESTED, STAGING, READY, TRANSFERRED, REPAIRED, FAILED;
+
+    public static ImmutableListMultimap<String, RepairStatus> statusByGroup() {
+        return new ImmutableListMultimap.Builder<String, RepairStatus>()
+                .put("Pending", RepairStatus.REQUESTED)
+                .put("Active", RepairStatus.STAGING)
+                .put("Active", RepairStatus.READY)
+                .put("Active", RepairStatus.TRANSFERRED)
+                .put("Success", RepairStatus.REPAIRED)
+                .put("Failure", RepairStatus.FAILED)
+                .build();
+    }
 }
