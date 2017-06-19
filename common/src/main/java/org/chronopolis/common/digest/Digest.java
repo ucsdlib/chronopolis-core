08f39280dd21b023e61c389e02ce0e10faf0a769
Michael Ritter <shake@umiacs.umd.edu>
2017-06-19 15:16:36 -0400

    Add the guava HashFunction so we can easily retrieve it

--- common/src/main/java/org/chronopolis/common/digest/Digest.java
+++ common/src/main/java/org/chronopolis/common/digest/Digest.java
@@ -1,23 +1,26 @@
 package org.chronopolis.common.digest;
 
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
 /**
- * TODO: Add HashFunction (Guava) to constructor
  *
  * Created by shake on 2/6/14.
  */
 public enum Digest {
 
-    SHA_256("SHA-256", "sha256"),
-    MD5("MD5", "md5");
+    SHA_256("SHA-256", "sha256", Hashing.sha256()),
+    MD5("MD5", "md5", Hashing.md5());
 
 
     private final String name;
     private final String bagitIdentifier;
+    private final HashFunction function;
 
-    Digest(final String name, final String bagitIdentifier) {
+    Digest(final String name, final String bagitIdentifier, HashFunction function) {
         this.name = name;
         this.bagitIdentifier = bagitIdentifier;
-
+        this.function = function;
     }
 
     public static Digest fromString(final String algorithm) {
@@ -50,4 +53,12 @@ public enum Digest {
         return bagitIdentifier;
     }
 
+    /**
+     * Get the HashFunction for the Digest
+     *
+     * @return the HashFunction
+     */
+    public HashFunction getFunction() {
+        return function;
+    }
 }
