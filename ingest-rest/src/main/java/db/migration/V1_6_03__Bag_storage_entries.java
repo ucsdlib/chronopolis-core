75ad10d79a06e542568688bf352ab3342858f296
Michael Ritter <shake@umiacs.umd.edu>
2017-07-14 17:57:16 -0400

    Update migration to populate fixity tables if information exists

--- ingest-rest/src/main/java/db/migration/V1_6_03__Bag_storage_entries.java
+++ ingest-rest/src/main/java/db/migration/V1_6_03__Bag_storage_entries.java
@@ -20,7 +20,7 @@ public class V1_6_03__Bag_storage_entries implements JdbcMigration {
 
     @Override
     public void migrate(Connection connection) throws Exception {
-        String sqlSelectBags = "SELECT id, location, tag_manifest_digest, token_location, token_digest, size, total_files FROM bag";
+        String sqlSelectBags = "SELECT id, location, tag_manifest_digest, token_location, token_digest, size, total_files, fixity_algorithm FROM bag";
         String sqlSelectBagRegion = "SELECT id FROM storage_region WHERE data_type = 'BAG'";
         String sqlSelectTokenRegion = "SELECT id FROM storage_region WHERE data_type = 'TOKEN'";
 
@@ -51,17 +51,20 @@ public class V1_6_03__Bag_storage_entries implements JdbcMigration {
                 String tokenDigest = bags.getString(5);
                 Long size = bags.getLong(6);
                 Long totalFiles = bags.getLong(7);
+                String algorithm = bags.getString(8);
 
-                Long bagStorageId = insertStorage(bagRegionId, location, tagDigest, size, totalFiles, connection);
+                Long bagStorageId = insertStorage(bagRegionId, location, size, totalFiles, connection);
+                insertFixity(bagStorageId, algorithm, tagDigest, connection);
 
                 // Maybe we shouldn't push storage for tokens?? I'm not really sure here, but we'll do it for now
                 // Can always be changed before the release
-                Long tokenStorageId = insertStorage(tokenRegionId, tokenLocation, tokenDigest, 1L, 1L, connection);
+                Long tokenStorageId = insertStorage(tokenRegionId, tokenLocation, 1L, 1L, connection);
 
                 // Regardless, a little check in case the tokens have not been written yet. Bag storage id _should_ always be nonnull
                 if (tokenStorageId == -1) {
                     updateBag(id, bagStorageId, connection);
                 } else {
+                    insertFixity(tokenStorageId, algorithm, tokenDigest, connection);
                     updateBagAndTokens(id, bagStorageId, tokenStorageId, connection);
                 }
             }
@@ -89,10 +92,24 @@ public class V1_6_03__Bag_storage_entries implements JdbcMigration {
         }
     }
 
-    private Long insertStorage(Long region, String path, String checksum, Long size, Long files, Connection connection) throws SQLException {
+    private void insertFixity(Long storage, String algorithm, String checksum, Connection connection) throws SQLException {
+        String sqlInsertFixity = "INSERT INTO fixity(id, storage_id, algorithm, value, created_at)" +
+                " VALUES(DEFAULT, ?, ?, ?, CURRENT_TIMESTAMP)";
+        if (checksum != null && !checksum.isEmpty()) {
+            try (PreparedStatement statement = connection.prepareStatement(sqlInsertFixity)) {
+                log.info("Pushing fixity with stuff: {} {}", algorithm, checksum);
+                statement.setLong(1, storage);
+                statement.setString(2, algorithm);
+                statement.setString(3, checksum);
+                statement.executeUpdate();
+            }
+        }
+    }
+
+    private Long insertStorage(Long region, String path, Long size, Long files, Connection connection) throws SQLException {
         Long key = -1L;
-        String sqlInsertStorage = "INSERT INTO storage(id, region_id, active, path, size, total_files, checksum, updated_at, created_at)" +
-                " VALUES(DEFAULT, ?, 'true', ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+        String sqlInsertStorage = "INSERT INTO storage(id, region_id, active, path, size, total_files, updated_at, created_at)" +
+                " VALUES(DEFAULT, ?, 'true', ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
 
         if (path != null && !path.isEmpty()) {
             ResultSet keys;
@@ -101,12 +118,10 @@ public class V1_6_03__Bag_storage_entries implements JdbcMigration {
                 statement.setString(2, path);
                 statement.setLong(3, size);
                 statement.setLong(4, files);
-                statement.setString(5, checksum);
                 statement.executeUpdate();
                 keys = statement.getGeneratedKeys();
                 if (keys.next()) {
                     key = keys.getLong(1);
-                    log.info("found key {}", key);
                 }
             }
         }
