1fc2790eaaf4394162f8bfb5fd430fd74fa4636a
Michael Ritter <shake@umiacs.umd.edu>
2017-08-09 14:28:56 -0400

    #49 - Move TokenTask to the tokenizer module

--- tokenizer/src/main/java/org/chronopolis/tokenize/scheduled/TokenTask.java
+++ tokenizer/src/main/java/org/chronopolis/tokenize/scheduled/TokenTask.java
@@ -1,31 +1,34 @@
-package org.chronopolis.ingest.task;
+package org.chronopolis.tokenize.scheduled;
 
+import com.google.common.collect.ImmutableMap;
 import org.chronopolis.common.ace.AceConfiguration;
 import org.chronopolis.common.concurrent.TrackingThreadPoolExecutor;
-import org.chronopolis.common.storage.TokenStagingProperties;
-import org.chronopolis.ingest.repository.BagRepository;
-import org.chronopolis.ingest.repository.TokenRepository;
+import org.chronopolis.common.storage.BagStagingProperties;
+import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.entities.Bag;
 import org.chronopolis.rest.models.BagStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import retrofit2.Call;
+import retrofit2.Response;
 
-import java.util.Collection;
+import java.io.IOException;
 
 /**
  * Basic task to submit threads for tokenization
- *
+ * <p>
  * Works on bags which have finished being INITIALIZED
  * so that they may be replicated
- *
+ * <p>
  * todo: when the TokenRunner is updated to no longer create tokens, we may be able to skip
- *       that work and simply spawn token writers here
- *
+ * that work and simply spawn token writers here
+ * <p>
  * Created by shake on 2/6/2015.
  */
 @Component
@@ -34,20 +37,17 @@ import java.util.Collection;
 public class TokenTask {
     private final Logger log = LoggerFactory.getLogger(TokenTask.class);
 
-    private final TokenRepository tokenRepository;
-    private final BagRepository repository;
+    private final IngestAPI ingest;
     private final AceConfiguration ace;
-    private final TokenStagingProperties properties;
+    private final BagStagingProperties properties;
     private final TrackingThreadPoolExecutor<Bag> tokenExecutor;
 
     @Autowired
-    public TokenTask(TokenRepository tokenRepository,
-                     BagRepository repository,
+    public TokenTask(IngestAPI ingest,
                      AceConfiguration ace,
-                     TokenStagingProperties properties,
+                     BagStagingProperties properties,
                      TrackingThreadPoolExecutor<Bag> tokenExecutor) {
-        this.tokenRepository = tokenRepository;
-        this.repository = repository;
+        this.ingest = ingest;
         this.ace = ace;
         this.properties = properties;
         this.tokenExecutor = tokenExecutor;
@@ -57,16 +57,16 @@ public class TokenTask {
     public void tokenize() {
         log.info("Searching for bags to tokenize");
 
-        Collection<Bag> bags = repository.findByStatus(BagStatus.INITIALIZED);
-        log.debug("Submitting {} bags", bags.size());
-        for (Bag bag : bags) {
-            TokenRunner runner = new TokenRunner(bag,
-                    ace.getIms(),
-                    "/dev/null",
-                    properties.getPosix().getPath(),
-                    repository,
-                    tokenRepository);
-            tokenExecutor.submitIfAvailable(runner, bag);
+        // Query ingest API
+        // Maybe getMyBags? Can work this out later
+        // Also need the storage region
+        Call<PageImpl<org.chronopolis.rest.models.Bag>> bags = ingest.getBags(
+                ImmutableMap.of("status", BagStatus.DEPOSITED,
+                        "region_id", properties.getPosix().getId()));
+        try {
+            Response<PageImpl<org.chronopolis.rest.models.Bag>> response = bags.execute();
+        } catch (IOException e) {
+            log.error("Error communicating with the ingest server", e);
         }
 
     }
