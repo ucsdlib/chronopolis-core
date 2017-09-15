a3e2d998c7aae023d193227a7cdb2009b71738f0
Michael Ritter <shake@umiacs.umd.edu>
2017-09-15 12:24:24 -0400

    #65 - Fix the root paths for token and repairs

--- rest-common/src/main/java/org/chronopolis/rest/api/Paths.java
+++ rest-common/src/main/java/org/chronopolis/rest/api/Paths.java
@@ -6,8 +6,8 @@ package org.chronopolis.rest.api;
 public class Paths {
 
     protected static final String BAG_ROOT = "api/bags";
-    protected static final String TOKEN_ROOT = "api/bags/{id}";
-    protected static final String REPAIR_ROOT = "api/bags/{id}";
+    protected static final String TOKEN_ROOT = "api/tokens";
+    protected static final String REPAIR_ROOT = "api/repairs";
     protected static final String STORAGE_ROOT = "api/storage";
     protected static final String STAGING_ROOT = "api/bags/{id}";
     protected static final String REPLICATION_ROOT = "api/replications";
