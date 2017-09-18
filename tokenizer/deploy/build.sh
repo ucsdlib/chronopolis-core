3273f3d261cf64258b907632b12df43c99e43c09
Michael Ritter <shake@umiacs.umd.edu>
2017-09-18 15:49:00 -0400

    Build script for creating a tokenizer package

---
+++ tokenizer/deploy/build.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+working_dir=$PWD
+sources_dir=tokenizer
+build_dir=TARS
+finaljar=$sources/tokenizer.jar
+retval=0
+
+if [ "$1" = "clean" ]; then
+    echo "Cleaning"
+    rm -rf tokenizer/
+    rm -rf TARS/
+    exit 0
+fi
+
+if [ ! -d $sources_dir ]; then
+    mkdir $sources_dir
+fi
+
+if [ ! -d $build_dir ]; then
+    mkdir $build_dir
+fi
+
+cd ../ 
+
+# Get the version of the build and trim off the -SNAPSHOT
+echo "Getting version from maven..."
+full_version=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
+version=`echo $full_version | sed 's/-.*//'`
+release_type=`echo $full_version | sed 's/.*-//'`
+
+if [ $? -ne 0 ]; then
+    echo "Error getting version from maven exec plugin"
+    exit
+fi
+
+jarfile=target/standalone/tokenizer-standalone-$version-$release_type.jar
+
+if [ ! -e $jarfile ]; then
+    echo "Building latest jar..."
+    exec mvn -q -Dmaven.test.redirectTestOutputToFile=true clean install # > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Error building tokenizer"
+        exit 99
+    fi
+else
+    echo "Jar already built"
+fi
+
+cp $jarfile $working_dir/$sources_dir/$finaljar
+cp target/classes/application.yml $working_dir/$sources_dir/application.yml
+
+cd $working_dir
+tar cvf $build_dir/tokenizer-$version-$release_type.tar $sources_dir
