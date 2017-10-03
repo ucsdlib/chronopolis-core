7f081331c6532ca748f9948eaac81287416c61e8
Michael Ritter <shake@umiacs.umd.edu>
2017-10-03 13:21:08 -0400

    #69 - Update install dir/jar location; Check user exists before executing

--- replication-shell/src/main/sh/replication.sh
+++ replication-shell/src/main/sh/replication.sh
@@ -18,15 +18,16 @@
 # User to execute as
 CHRON_USER="chronopolis"
 
-REPL_JAR="/usr/lib/chronopolis/replication.jar"
+REPL_DIR="/usr/local/chronopolis/replication"
+REPL_JAR="replication.jar"
 REPL_PID_FILE="/var/run/replication.pid"
 
 # Set the location which holds our application.properties
 # SPRING_CONFIG_NAME="settings.conf"
-SPRING_CONFIG_LOCATION="/etc/chronopolis/"
+SPRING_CONFIG_LOCATION="$REPL_DIR/"
 
 JAVA_BIN=/usr/bin/java
-JAVA_CMD="$JAVA_BIN -jar $REPL_JAR"
+JAVA_CMD="$JAVA_BIN -jar $REPL_DIR/$REPL_JAR"
 PARAMS="--spring.config.location=$SPRING_CONFIG_LOCATION &"
 
 . /etc/init.d/functions
@@ -36,6 +37,12 @@ COUNTDOWN=1
 
 case "$1" in
     start)
+    if ! getent passwd $CHRON_USER 2>&1; then
+        echo "User $CHRON_USER does not exist; unable to start replication service"
+        action "Started Replication Service " /bin/false
+        exit 2
+    fi
+
     echo "Starting the replication service"
     daemon --user "$CHRON_USER" --pidfile "$REPL_PID_FILE" $JAVA_CMD $PARAMS > /dev/null 2>&1
     RETVAL=$?
