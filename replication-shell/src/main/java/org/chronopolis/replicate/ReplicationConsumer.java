1b038afef4a42ca2f320bde38b813466d5b4a4bf
Michael Ritter <shake@umiacs.umd.edu>
2017-10-04 16:32:39 -0400

    #68/#69 - Write pids for processes

--- replication-shell/src/main/java/org/chronopolis/replicate/ReplicationConsumer.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/ReplicationConsumer.java
@@ -3,14 +3,13 @@ package org.chronopolis.replicate;
 import org.chronopolis.replicate.config.ReplicationConfig;
 import org.chronopolis.replicate.scheduled.ReplicationQueryTask;
 import org.chronopolis.replicate.service.ReplicationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
@@ -31,7 +30,6 @@ import org.springframework.stereotype.Component;
 })
 @EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class ReplicationConsumer implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(ReplicationConsumer.class);
 
     private final ReplicationService service;
 
@@ -41,8 +39,9 @@ public class ReplicationConsumer implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        log.debug("Started with args: {}", args);
-        SpringApplication.exit(SpringApplication.run(ReplicationConsumer.class, args));
+        SpringApplication app = new SpringApplication(ReplicationConsumer.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        SpringApplication.exit(app.run(args));
     }
 
     @Override
