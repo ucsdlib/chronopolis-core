b314dbde15907384a609709567ca70ca98339e1e
Michael Ritter <shake@umiacs.umd.edu>
2017-10-04 16:32:19 -0400

    #68/#69 - Clean up initd scripts; better logic for starting/stopping

--- ingest-rest/src/main/sh/ingest-server.sh
+++ ingest-rest/src/main/sh/ingest-server.sh
@@ -15,47 +15,66 @@
 # Description:   Start the Chronopolis Ingest Server
 ### END INIT INFO
 
-# Amount of time to attempt to communicate with the serve
-TIMEOUT=15
+# Amount of time to attempt to communicate with the server
+TIMEOUT=45
 
 # User to execute as
 CHRON_USER="chronopolis"
 
 INGEST_DIR="/usr/local/chronopolis/ingest"
 INGEST_JAR="ingest-server.jar"
-INGEST_PID_FILE="/var/run/ingest-server.pid"
 
-# todo: see if we need to keep this env variable around
-SPRING_CONFIG_LOCATION="$INGEST_DIR/"
 
 JAVA_BIN=/usr/bin/java
-JAVA_CMD="$JAVA_BIN -jar $INGEST_DIR/$INGEST_JAR"
-PARAMS="--spring.config.location=$SPRING_CONFIG_LOCATION &"
+JAVA_CMD="$JAVA_BIN -jar $INGEST_DIR/$INGEST_JAR &"
 
 . /etc/init.d/functions
 
 RETVAL=0
 
-# This needs to be updated to start/stop correctly...
-# start todos:
-# - lockfile
-# - pidfile 
-# stop todos:
-# - lockfile
-# - check status of kill
+# vars for our use
+prog="ingest-server"
+pidfile="/var/run/ingest-server.pid"
+lockfile=/var/lock/subsys/ingest-server
 
-case "$1" in
-    start)
-    if ! getent passwd "$CHRON_USER" 2>&1; then
+# env vars for spring
+export SPRING_PID_FILE=$pidfile
+export SPRING_CONFIG_LOCATION="$INGEST_DIR/"
+
+start(){
+    ret=0
+    # check if the user exists
+    if ! getent passwd "$CHRON_USER" > /dev/null 2>&1; then
         echo "User $CHRON_USER does not exist; unable to start Ingest Server"
-        action "Started Ingest Server: " /bin/false
-        exit 2
+        action $"Starting $prog: " /bin/false
+        return 2
     fi
 
-    echo "Starting the ingest server"
-    daemon --user "$CHRON_USER" --pidfile "$REPL_PID_FILE" $JAVA_CMD $PARAMS > /dev/null 2>&1
+    # check if we're already running
+    RUNNING=0
+    if [ -f "$pidfile" ]; then
+        PID=`cat "$pidfile" 2>/dev/null`
+        if [ -n "$PID" ] && [ -d "/proc/$PID" ]; then
+            RUNNING=1
+        fi
+    else
+        # create the pidfile and grant ownership
+        touch "$pidfile"
+        chown "$CHRON_USER":"$CHRON_USER" "$pidfile"
+    fi
 
-    echo "Attempting to connect to server..."
+    # check for failed/hung server
+    # may need to update host endpoint
+    RESPONSE=`curl -I -X GET http://localhost:8080 > /dev/null 2>&1`
+    if [ $RUNNING = 1 ] && [ $? = 0 ]; then
+        action $"Starting $prog: " /bin/true
+        return 0
+    fi
+
+    # exec
+    daemon --user "$CHRON_USER" --pidfile "$pidfile" $JAVA_CMD
+
+    # wait for timeout
     while [ $TIMEOUT -gt 0 ]; do
         # Try to connect to the server
         curl -I -X GET http://localhost:8080 > /dev/null 2>&1
@@ -66,37 +85,63 @@ case "$1" in
         let TIMEOUT=${TIMEOUT}-1
     done
 
-    # We could also use the value of the timeout
-    RETVAL=$?
-
-    # This bit is from the jenkins init script, I'm not sure if we'll need it though
-    if [ $RETVAL -eq 0 ]; then
-        /bin/ps hww -u "$CHRON_USER" -o sess,ppid,pid,cmd | \
-        while read sess ppid pid cmd; do
-        [ $ppid -eq 1 ] || continue
-        echo "$cmd" | grep $REPL_JAR > /dev/null
-        [ $? -eq 0 ] || continue
-        echo $pid > $REPL_PID_FILE
-        done
-        action "Started Ingest Server " /bin/true
+    if [ $? -eq 0 ]; then
+        action $"Starting $prog: " /bin/true
+        touch $lockfile
     else
-        action "Started Ingest Server " /bin/false
+        action $"Starting $prog: " /bin/false
+        ret=3
     fi
-    echo
-    RETVAL=$?
-    ;;
+
+    return $ret
+}
+
+stop(){
+    ret=0
+
+    # No pidfile = not running
+    if [ ! -f "$pidfile" ]; then
+        action $"Stopping $prog: " /bin/true
+        return 0
+    fi
+
+    # Get the pid and attempt to kill
+    PID=`cat "$pidfile" 2>/dev/null`
+    if [ -n "$PID" ]; then
+        /bin/kill "$PID" > /dev/null 2>&1 || break
+        if [ $? -eq 0 ]; then
+            action $"Stopping $prog: " /bin/true
+            rm -f $lockfile
+            rm -f "$pidfile"
+        else
+            action $"Stopping $prog: " /bin/false
+            ret=4
+        fi
+    else
+        action $"Stopping $prog: " /bin/false
+        ret=4
+    fi
+
+    return $ret
+}
+
+case "$1" in
+    start)
+        start
+        ;;
     stop)
-    killproc ingest-server
-    action "Stopped Ingest Server" /bin/true
-    ;;
+        stop
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
     reload)
-    $0 stop
-    $0 start
-    ;;
+        exit 5
+        ;;
     status)
-        status ingest-server
-        RETVAL=$?
-    ;;
+        status -p "$pidfile" ingest-server
+        ;;
 esac
 
-exit $RETVAL
+exit $?
