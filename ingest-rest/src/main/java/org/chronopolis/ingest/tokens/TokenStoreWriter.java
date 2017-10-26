cf3ff45b81f57d70b691dd436b415f5d4e685801
Michael Ritter <shake@umiacs.umd.edu>
2017-10-26 16:27:48 -0400

    Remove redundant array creation

--- ingest-rest/src/main/java/org/chronopolis/ingest/tokens/TokenStoreWriter.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/tokens/TokenStoreWriter.java
@@ -95,7 +95,7 @@ public class TokenStoreWriter implements Runnable {
             boolean next = true;
             while (next) {
                 log.debug("Iterating page # {} size {} offset {}",
-                        new Object[]{pageable.getPageNumber(), pageable.getPageSize(), pageable.getOffset()});
+                        pageable.getPageNumber(), pageable.getPageSize(), pageable.getOffset());
                 Page<AceToken> tokens = tokenService.findAll(new AceTokenSearchCriteria().withBagId(bag.getId()),
                         pageable);
 
