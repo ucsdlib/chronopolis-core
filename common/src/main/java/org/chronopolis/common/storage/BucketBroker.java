6d56e92106f76821dad58602ad35070959e547cb
Michael Ritter <shake@umiacs.umd.edu>
2017-11-13 15:31:34 -0500

    #54 - Instantiation of BucketBrokers; javadoc

--- common/src/main/java/org/chronopolis/common/storage/BucketBroker.java
+++ common/src/main/java/org/chronopolis/common/storage/BucketBroker.java
@@ -1,11 +1,18 @@
 package org.chronopolis.common.storage;
 
-import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanCreationException;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
@@ -13,15 +20,57 @@ import static java.util.Comparator.reverseOrder;
 
 /**
  * Class to determine the best bucket to place an operation in
+ * <p>
+ * Should this be an interface?
  *
  * @author shake
  */
 public class BucketBroker {
 
-    private List<Bucket> buckets;
+    private static final Logger log = LoggerFactory.getLogger(BucketBroker.class);
 
+    private final List<Bucket> buckets;
+
+    private BucketBroker() {
+        this.buckets = new CopyOnWriteArrayList<>();
+    }
+
+    /**
+     * Create a BucketBroker from the configuration used for defining the
+     * Preservation Storage
+     *
+     * @param preservationProperties the configuration for preservation storage systems
+     * @return a BucketBroker for managing StorageOperations over said preservation storage
+     * @throws BeanCreationException if a Bucket can not be created for given configuration parameters
+     */
     public static BucketBroker fromProperties(PreservationProperties preservationProperties) {
         BucketBroker broker = new BucketBroker();
+
+        // I guess we'll need to do this for each type of storage
+        // should probably be a private static method
+        preservationProperties.getPosix().forEach(posix -> {
+            Path root = Paths.get(posix.getPath());
+            try {
+                broker.addBucket(new PosixBucket(posix, Files.getFileStore(root)));
+            } catch (IOException e) {
+                // Not sure if we should push this to the constructor or do it here
+                log.error("[{}] Error creating Bucket", posix.getPath(), e);
+                throw new BeanCreationException(e.getMessage());
+            }
+        });
+
+        return broker;
+    }
+
+    /**
+     * Return a bucket broker which manages only a single bucket. Useful for testing.
+     *
+     * @param bucket The bucket to manager
+     * @return a new BucketBroker
+     */
+    public static BucketBroker forBucket(Bucket bucket) {
+        BucketBroker broker = new BucketBroker();
+        broker.addBucket(bucket);
         return broker;
     }
 
@@ -49,11 +98,26 @@ public class BucketBroker {
                 .findFirst();
     }
 
+    /**
+     * Return a weighted stream of Buckets for a given storage operation
+     * todo: test this
+     *
+     * @param operation the operation we're looking for
+     * @return a stream of buckets, weighted by how likely they are to contain the operation
+     */
     private Stream<Bucket> weighBuckets(StorageOperation operation) {
         return buckets.stream()
                 .sorted(comparing(bucket -> weight(bucket, operation), reverseOrder()));
     }
 
+    /**
+     * Retrieve the "weight" for a Bucket. I.e. given a StorageOperation, how likely
+     * the bucket is to contain the Operation (not mapped on [0,1], however).
+     *
+     * @param bucket    the bucket to weight
+     * @param operation the operation to check for
+     * @return the weight
+     */
     private Long weight(Bucket bucket, StorageOperation operation) {
         String hashString = bucket.hashCode() + operation.getIdentifier();
         return Hashing.sha256()
@@ -61,42 +125,15 @@ public class BucketBroker {
                 .asLong();
     }
 
-    public BucketBroker setBuckets(List<Bucket> buckets) {
-        this.buckets = buckets;
-        return this;
-    }
-
-    public BucketBroker addBucket(Bucket bucket) {
+    /**
+     * Add a bucket which can be used for various StorageOperations
+     *
+     * @param bucket the bucket to add
+     * @return this BucketBroker
+     */
+    private BucketBroker addBucket(Bucket bucket) {
         this.buckets.add(bucket);
         return this;
     }
 
-    private class Weight {
-        private final int bucket;
-        private final HashCode hashCode;
-        private final StorageOperation operation;
-
-        private Weight(int bucket, StorageOperation operation) {
-            this.bucket = bucket;
-            this.operation = operation;
-
-            // ummm not terrible I guess but not great either
-            this.hashCode = Hashing.sha256().newHasher()
-                    .putInt(bucket)
-                    .putString(operation.getIdentifier(), Charset.defaultCharset())
-                    .hash();
-        }
-
-        public int getBucket() {
-            return bucket;
-        }
-
-        public HashCode getHashCode() {
-            return hashCode;
-        }
-
-        public StorageOperation getOperation() {
-            return operation;
-        }
-    }
 }
