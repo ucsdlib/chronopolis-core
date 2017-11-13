ab352a90d6800e964a10504789a4b165209b3f5d
Michael Ritter <shake@umiacs.umd.edu>
2017-11-13 15:46:16 -0500

    #54 - Update bean config

--- replication-shell/src/main/java/org/chronopolis/replicate/config/ReplicationConfig.java
+++ replication-shell/src/main/java/org/chronopolis/replicate/config/ReplicationConfig.java
@@ -1,54 +1,40 @@
 package org.chronopolis.replicate.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Response;
 import org.chronopolis.common.ace.AceConfiguration;
 import org.chronopolis.common.ace.AceService;
 import org.chronopolis.common.mail.MailUtil;
 import org.chronopolis.common.mail.SmtpProperties;
+import org.chronopolis.common.storage.BucketBroker;
 import org.chronopolis.common.storage.PreservationProperties;
 import org.chronopolis.common.storage.PreservationPropertiesValidator;
 import org.chronopolis.replicate.ReplicationProperties;
 import org.chronopolis.replicate.batch.Submitter;
 import org.chronopolis.rest.api.ErrorLogger;
-import org.chronopolis.rest.api.IngestAPI;
 import org.chronopolis.rest.api.IngestAPIProperties;
-import org.chronopolis.rest.models.Bag;
-import org.chronopolis.rest.models.Replication;
+import org.chronopolis.rest.api.IngestGenerator;
+import org.chronopolis.rest.api.ServiceGenerator;
 import org.chronopolis.rest.support.OkBasicInterceptor;
-import org.chronopolis.rest.support.PageDeserializer;
-import org.chronopolis.rest.support.ZonedDateTimeDeserializer;
-import org.chronopolis.rest.support.ZonedDateTimeSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.validation.Validator;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.time.ZonedDateTime;
-import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Configuration for the beans used by the replication-shell
- *
+ * <p>
  * Created by shake on 4/16/14.
  */
 @Configuration
-@SuppressWarnings("ALL")
 @EnableConfigurationProperties({SmtpProperties.class,
         PreservationProperties.class,
         ReplicationProperties.class,
@@ -57,10 +43,10 @@ public class ReplicationConfig {
     public final Logger log = LoggerFactory.getLogger(ReplicationConfig.class);
 
     @Value("${debug.retrofit:NONE}")
-    String retrofitLogLevel;
+    public String retrofitLogLevel;
 
     @Value("${ace.timeout:5}")
-    Long timeout;
+    public Long timeout;
 
     /**
      * Logger to capture why errors happened in Retrofit
@@ -68,18 +54,18 @@ public class ReplicationConfig {
      * @return
      */
     @Bean
-    ErrorLogger logger() {
+    public ErrorLogger logger() {
         return new ErrorLogger();
     }
 
     /**
      * Retrofit adapter for interacting with the ACE REST API
      *
-     * @param aceSettings - Settings to connect to ACE with
-     * @return
+     * @param configuration the ACE AM configuration properties
+     * @return the AceService for connecting to ACE
      */
     @Bean
-    AceService aceService(AceConfiguration configuration) {
+    public AceService aceService(AceConfiguration configuration) {
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new OkBasicInterceptor(configuration.getUsername(), configuration.getPassword()))
                 .readTimeout(timeout, TimeUnit.MINUTES)
@@ -98,84 +84,55 @@ public class ReplicationConfig {
     }
 
     /**
-     * Retrofit adapter for interacting with the ingest-server REST API
+     * ServiceGenerator for creating services which can send requests to the Ingest REST API
      *
-     * @param apiSettings - Settings to connect to the ingest-server with
+     * @param properties the API properties for configuration
+     * @return the ServiceGenerator
+     */
+    @Bean
+    public ServiceGenerator serviceGenerator(IngestAPIProperties properties) {
+        return new IngestGenerator(properties);
+    }
+
+    /**
+     * The main replication submission bean
+     *
+     * @param mail          the mail utility for sending success/failure notifications
+     * @param ace           the service to connect to the ACE-AM REST API
+     * @param configuration the configuration properties for ACE-AM
+     * @param properties    the configuration for... general replication properties
+     * @param generator     the ServiceGenerator to use for creating Ingest API services
+     * @param broker        the BucketBroker for handling distribution of replications into Buckets
      * @return
      */
     @Bean
-    IngestAPI ingestAPI(IngestAPIProperties properties) {
-        String endpoint = properties.getEndpoint();
-        if (!endpoint.endsWith("/")) {
-            endpoint = endpoint + "/";
-        }
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new Interceptor() {
-                    @Override
-                    public Response intercept(Chain chain) throws IOException {
-                        for (String s : chain.request().headers().names()) {
-                            log.info(chain.request().headers().get(s));
-                        }
-                        return chain.proceed(chain.request());
-                    }
-                })
-                .addNetworkInterceptor(new OkBasicInterceptor(properties.getUsername(),
-                        properties.getPassword()))
-                .build();
-
-        Type bagPage = new TypeToken<PageImpl<Bag>>() {}.getType();
-        Type bagList = new TypeToken<List<Bag>>() {}.getType();
-        Type replPage = new TypeToken<PageImpl<Replication>>() {}.getType();
-        Type replList = new TypeToken<List<Replication>>() {}.getType();
-
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(bagPage, new PageDeserializer(bagList))
-                .registerTypeAdapter(replPage, new PageDeserializer(replList))
-                .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeSerializer())
-                .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeDeserializer())
-                .create();
-
-        Retrofit adapter = new Retrofit.Builder()
-                .baseUrl(endpoint)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                // .setErrorHandler(logger())
-                // .setLogLevel(Retrofit.LogLevel.valueOf(retrofitLogLevel))
-                // .setLogLevel(Retrofit.LogLevel.FULL)
-                .build();
-
-        return adapter.create(IngestAPI.class);
+    public Submitter submitter(MailUtil mail,
+                               AceService ace,
+                               AceConfiguration configuration,
+                               ReplicationProperties properties,
+                               ServiceGenerator generator,
+                               BucketBroker broker) {
+        return new Submitter(mail, ace, broker, generator, configuration, properties, io(), http());
     }
 
     @Bean
-    Submitter submitter(MailUtil mail,
-                        AceService ace,
-                        IngestAPI ingest,
-                        AceConfiguration configuration,
-                        PreservationProperties preservationProperties,
-                        ReplicationProperties properties) {
-        return new Submitter(mail, ace, ingest, preservationProperties, configuration, properties, io(), http());
-    }
-
-    @Bean
-    ThreadPoolExecutor http() {
+    public ThreadPoolExecutor http() {
         return new ThreadPoolExecutor(4, 8, 30, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
     }
 
     @Bean
-    ThreadPoolExecutor io() {
+    public ThreadPoolExecutor io() {
         return new ThreadPoolExecutor(2, 2, 30, TimeUnit.SECONDS, new LinkedBlockingDeque<>());
     }
 
     /**
      * Class to send email notifications regarding replications
      *
-     * @param smtpSettings - The settings to use for smtp messages
-     * @return
+     * @param properties the SMTP configuration properties
+     * @return the MailUtil helper
      */
     @Bean
-    MailUtil mailUtil(SmtpProperties properties) {
+    public MailUtil mailUtil(SmtpProperties properties) {
         MailUtil mailUtil = new MailUtil();
         mailUtil.setSmtpFrom(properties.getFrom());
         mailUtil.setSmtpTo(properties.getTo());
@@ -184,6 +141,23 @@ public class ReplicationConfig {
         return mailUtil;
     }
 
+    /**
+     * Our BucketBroker to determine placement of Replications
+     *
+     * @param preservationProperties the properties containing our Storage Spaces
+     * @return the BucketBroker
+     */
+    @Bean
+    public BucketBroker bucketBroker(PreservationProperties preservationProperties) {
+        return BucketBroker.fromProperties(preservationProperties);
+    }
+
+    /**
+     * Validator to make sure the PreservationProperties contain storage areas exist, can be
+     * read from, and can be written to.
+     *
+     * @return the PreservationPropertiesValidator
+     */
     @Bean
     static Validator configurationPropertiesValidator() {
         return new PreservationPropertiesValidator();
