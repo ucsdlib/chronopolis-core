d6a6bd4364e457ee2bfc2ecc6c725a01053fd5c5
Michael Ritter <shake@umiacs.umd.edu>
2017-11-16 16:47:31 -0500

    Helper method to create OperationTypes from a String

--- common/src/main/java/org/chronopolis/common/storage/OperationType.java
+++ common/src/main/java/org/chronopolis/common/storage/OperationType.java
@@ -2,7 +2,7 @@ package org.chronopolis.common.storage;
 
 /**
  * Some StorageOperation types
- *
+ * <p>
  * Only rsync for now, might want separate things
  * for staging/https/ftp/etc
  *
@@ -10,6 +10,16 @@ package org.chronopolis.common.storage;
  */
 public enum OperationType {
 
-    RSYNC, NOP
+    RSYNC, NOP;
 
-}
+    public static OperationType from(String string) {
+        switch (string) {
+            case "rsync":
+            case "RSYNC":
+                return RSYNC;
+            default:
+                return NOP;
+        }
+    }
+
+    }
