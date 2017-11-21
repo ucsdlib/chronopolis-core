78b93a4430517e1f16a70c3b4e4b15b7d3deab06
Michael Ritter <shake@umiacs.umd.edu>
2017-11-21 15:57:47 -0500

    #74 - Add method to retrieve active StagingStorage entities for a Bag

--- ingest-rest/src/main/java/org/chronopolis/ingest/repository/dao/StagingService.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/repository/dao/StagingService.java
@@ -1,12 +1,17 @@
 package org.chronopolis.ingest.repository.dao;
 
+import com.querydsl.core.types.CollectionExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.chronopolis.ingest.repository.StorageRepository;
+import org.chronopolis.rest.entities.Bag;
+import org.chronopolis.rest.entities.QBag;
 import org.chronopolis.rest.entities.storage.QFixity;
+import org.chronopolis.rest.entities.storage.QStagingStorage;
 import org.chronopolis.rest.entities.storage.StagingStorage;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Hey do some stuff look at me I'm a data access object woo
@@ -21,6 +26,25 @@ public class StagingService extends SearchService<StagingStorage, Long, StorageR
     public StagingService(StorageRepository storageRepository, EntityManager manager) {
         super(storageRepository);
         this.manager = manager;
+    }
+
+    /**
+     * Get the active storage object for a bag
+     *
+     * @param bag the bag whose staging_storage we're retrieving
+     * @param storageJoin
+     * @return the StagingStorage object, if found
+     */
+    public Optional<StagingStorage> activeStorageForBag(Bag bag, CollectionExpression<?, StagingStorage> storageJoin) {
+        JPAQueryFactory factory = new JPAQueryFactory(manager);
+        QBag b = QBag.bag;
+        QStagingStorage storage = QStagingStorage.stagingStorage;
+        // once again not the best query but that's ok
+        return Optional.ofNullable(factory.from(b)
+                .innerJoin(storageJoin, storage)
+                .where(storage.active.isTrue().and(b.id.eq(bag.getId())))
+                .select(storage)
+                .fetchOne());
     }
 
     /**
