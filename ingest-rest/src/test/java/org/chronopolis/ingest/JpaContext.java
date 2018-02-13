75805836cbd27d3e31a09e6921a6588b87e9b1ae
Michael Ritter <shake@umiacs.umd.edu>
2018-02-13 13:11:24 -0500

    Have two separate DataSource beans: one local; one gitlab

--- ingest-rest/src/test/java/org/chronopolis/ingest/JpaContext.java
+++ ingest-rest/src/test/java/org/chronopolis/ingest/JpaContext.java
@@ -4,10 +4,14 @@ import com.opentable.db.postgres.embedded.FlywayPreparer;
 import com.opentable.db.postgres.embedded.PreparedDbProvider;
 import org.chronopolis.ingest.repository.Authority;
 import org.chronopolis.rest.entities.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -30,17 +34,28 @@ public class JpaContext {
     }
 
     @Bean
-    public DataSource dataSource() throws SQLException {
+    @Profile("!gitlab")
+    public DataSource embeddedDataSource() throws SQLException {
         FlywayPreparer preparer = FlywayPreparer.forClasspathLocation("db/schema");
         PreparedDbProvider provider = PreparedDbProvider.forPreparer(preparer);
         return provider.createDataSource();
     }
 
     @Bean
-    // This is for accessing and updating our users
+    @Profile("gitlab")
+    public DataSource serviceDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://postgres/ingest-test")
+                .username("runner")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean
     public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
         JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
         manager.setDataSource(dataSource);
         return manager;
     }
+
 }
