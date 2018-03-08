9fe736950a93b3335899606eeff0d5114bfb4f31
Michael Ritter <shake@umiacs.umd.edu>
2018-03-08 17:27:42 -0500

    FindOne/All support for Predicates

--- ingest-rest/src/main/java/org/chronopolis/ingest/repository/dao/PagedDAO.java
+++ ingest-rest/src/main/java/org/chronopolis/ingest/repository/dao/PagedDAO.java
@@ -1,6 +1,9 @@
 package org.chronopolis.ingest.repository.dao;
 
+import com.querydsl.core.QueryModifiers;
 import com.querydsl.core.types.EntityPath;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.chronopolis.ingest.models.Paged;
@@ -16,14 +19,12 @@ import java.util.List;
 /**
  * DAO to support basic fetching, saving, and removal from an EntityManager. Can be extended to
  * provide additional support for queries if any are needed.
- *
+ * <p>
  * Note that some deprecated methods are still called here while we require less of
  * Spring for JPA work. Soon we'll need to replace that with our own utilities I guess, or see
  * if there are some lightweight libraries for Pagination. Either way it should only be a class
  * or two so not much.
- *
- * todo: would it be better to send a predicate instead of a filter?
- *       maybe have support for both
+ * <p>
  *
  * @author shake
  */
@@ -35,6 +36,14 @@ public class PagedDAO {
         this.em = em;
     }
 
+    /**
+     * Find a single entity based on a filter (which should give a unique result)
+     *
+     * @param path   the EntityPath to query on
+     * @param filter the filter containing the query parameters
+     * @param <T>    the Type of the Entity
+     * @return the entity retrieved from the database or null if it does not exist
+     */
     public <T extends PersistableEntity> T findOne(EntityPath<T> path, Paged filter) {
         JPAQueryFactory factory = new JPAQueryFactory(em);
         return factory.selectFrom(path)
@@ -42,11 +51,84 @@ public class PagedDAO {
                 .fetchOne();
     }
 
+    /**
+     * Find a single entity based on a Predicate
+     *
+     * @param path      the EntityPath to query on
+     * @param predicate the predicate containing the query parameters
+     * @param <T>       the Type of the Entity
+     * @return the entity retrieved from the database or null if it does not exist
+     */
+    public <T extends PersistableEntity> T findOne(EntityPath<T> path,
+                                                   Predicate predicate) {
+        JPAQueryFactory factory = new JPAQueryFactory(em);
+        return factory.selectFrom(path)
+                .where(predicate)
+                .fetchOne();
+    }
+
+    /**
+     * Find all entities of a certain type
+     * <p>
+     * Normally receiving a Page instead of a List is preferred; this should only be used when
+     * absolutely necessary.
+     *
+     * @param path the EntityPath to query on
+     * @param <T>  the Type of the Entity
+     * @return a List containing all entities
+     */
     public <T extends PersistableEntity> List<T> findAll(EntityPath<T> path) {
         JPAQueryFactory factory = new JPAQueryFactory(em);
         return factory.selectFrom(path).fetch();
     }
 
+    /**
+     * Find all entities of a certain type matching a Predicate
+     *
+     * @param path the EntityPath to query on
+     * @param predicate the Predicate to filter on
+     * @param <T> the Type of the Entity
+     * @return a List containing all Entities that match the Predicate
+     */
+    public <T extends PersistableEntity> List<T> findAll(EntityPath<T> path, Predicate predicate) {
+        JPAQueryFactory factory = new JPAQueryFactory(em);
+        return factory.selectFrom(path)
+                .where(predicate)
+                .fetch();
+    }
+
+    /**
+     * Find all entities of a certain type matching a Predicate and restrict/order based on a given
+     * modifier and specifier.
+     *
+     * @param path the EntityPath to query on
+     * @param predicate the Predicate to filter on
+     * @param specifier the OrderSpecifier to sort the data
+     * @param modifiers the QueryModifiers to restrict/limit data (replicate paging)
+     * @param <T> the Type of the Entity
+     * @return a List containing all Entities that match the Predicate
+     */
+    public <T extends PersistableEntity> List<T> findAll(EntityPath<T> path,
+                                                         Predicate predicate,
+                                                         OrderSpecifier specifier,
+                                                         QueryModifiers modifiers) {
+        JPAQueryFactory factory = new JPAQueryFactory(em);
+        return factory.selectFrom(path)
+                .where(predicate)
+                .orderBy(specifier)
+                .restrict(modifiers)
+                .fetch();
+    }
+
+    /**
+     * Find entities of a certain type, limited in scope based on a filter which should give both
+     * query parameters and size restrictions for the ResultSet
+     *
+     * @param path   the EntityPath to query on
+     * @param filter the filter containing the query parameters
+     * @param <T>    the Type of the Entity
+     * @return a Page containing the ResultSet from querying
+     */
     public <T extends PersistableEntity> Page<T> findPage(EntityPath<T> path, Paged filter) {
         PageRequest req = filter.createPageRequest();
 
@@ -62,13 +144,13 @@ public class PagedDAO {
 
     /**
      * Save an Entity to the database using the EntityManager
-     *
+     * <p>
      * This combines em.persist and em.merge into one method. If the entity does not have an ID,
      * it will be persisted; otherwise it will be merged.
+     * <p>
+     * Note that for the moment transaction management is handled by Spring
      *
-     * Note that for the moment, transaction management is handled by Spring
-     *
-     * @param t The entity to merge
+     * @param t   The entity to merge
      * @param <T> The type of the entity. Must extend PersistableEntity in order to get the id
      */
     @Transactional
@@ -84,10 +166,10 @@ public class PagedDAO {
 
     /**
      * Remove an entity from the database using the EntityManager
+     * <p>
+     * Note that for the moment transaction management is handled by Spring
      *
-     * Note that for the moment, transaction management is handled by Spring
-     *
-     * @param t the entity to remove
+     * @param t   the entity to remove
      * @param <T> the type of the entity
      */
     @Transactional
