c492b5ec1d15ac1dd042e289195c12e5e2e1be99
Michael Ritter <shake@umiacs.umd.edu>
2018-06-11 12:59:33 -0400

    MQ Consumers for TokenRequestBatch and TokenRegistrar

---
+++ tokenizer-mq/src/main/java/org/chronopolis/tokenize/mq/artemis/ArtemisTokenRegistrar.java
@@ -0,0 +1,178 @@
+package org.chronopolis.tokenize.mq.artemis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.umiacs.ace.ims.api.IMSUtil;
+import edu.umiacs.ace.ims.ws.TokenResponse;
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.api.core.client.ClientConsumer;
+import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.api.core.client.ClientSession;
+import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
+import org.apache.activemq.artemis.api.core.client.ServerLocator;
+import org.chronopolis.rest.api.TokenService;
+import org.chronopolis.rest.models.AceTokenModel;
+import org.chronopolis.rest.models.Bag;
+import org.chronopolis.tokenize.mq.RegisterMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Simple consumer which registers tokens
+ *
+ * @author shake
+ */
+public class ArtemisTokenRegistrar implements Runnable, Closeable {
+    private final Logger log = LoggerFactory.getLogger(ArtemisTokenRegistrar.class);
+
+    private final TokenService tokens;
+    private final ServerLocator serverLocator;
+    private final AtomicBoolean running = new AtomicBoolean(true);
+
+    public ArtemisTokenRegistrar(TokenService tokens,
+                                 ServerLocator serverLocator) {
+        this.tokens = tokens;
+        this.serverLocator = serverLocator;
+    }
+
+    private void consume(ClientConsumer consumer, ClientSession session) {
+        // we might want to listen on a more general address, such as processing.#.register
+        // this would let us track bags which are still in progress
+        // e.g. processing.depositor0-bag0.register
+        try {
+            ClientMessage message = consumer.receive(1000);
+            if (message != null) {
+                String id = message.getStringProperty("id");
+                if (id != null && !id.isEmpty()) {
+                    message.individualAcknowledge();
+                    boolean rollback = registerToken(message);
+
+                    if (rollback) {
+                        session.rollback();
+                    }
+                }
+            }
+        } catch (ActiveMQException e) {
+            log.error("Exception communicating to message broker", e);
+        }
+    }
+
+    private boolean registerToken(ClientMessage clientMessage) {
+        Bag bag;
+        TokenResponse tokenResponse;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String text = clientMessage.getReadOnlyBodyBuffer().readString();
+            RegisterMessage message = mapper.readValue(text, RegisterMessage.class);
+            bag = message.getBag();
+            tokenResponse = message.getToken();
+        } catch (IOException e) {
+            log.error("Error deserializing received message", e);
+            return false;
+        }
+
+        // If this fails, it will look as though the program is hanging here when in fact it's
+        // probably a NoClassDef exception because the ace-ims-api package relies on log4j
+        String proof = IMSUtil.formatProof(tokenResponse);
+        Optional<String> filename = getFilename(tokenResponse);
+        Optional<AceTokenModel> optModel = filename.map(name -> new AceTokenModel()
+                .setProof(proof)
+                .setFilename(name)
+                .setBagId(bag.getId())
+                .setImsHost("ims.umiacs.umd.edu")
+                .setRound(tokenResponse.getRoundId())
+                .setAlgorithm(tokenResponse.getDigestService())
+                .setImsService(tokenResponse.getTokenClassName())
+                .setCreateDate(tokenResponse.getTimestamp().toGregorianCalendar().toZonedDateTime()));
+
+        return optModel.map(model -> tokens.createToken(bag.getId(), model))
+                .map(call -> accept(call, tokenResponse))
+                .orElse(false);
+    }
+
+    /**
+     * Match a TokenResponse's name against a known regex in order to extract the filename
+     *
+     * @param tokenResponse the token response to match against
+     * @return the filename, if present
+     */
+    private Optional<String> getFilename(TokenResponse tokenResponse) {
+        Pattern pattern = Pattern.compile("\\(.*?,.*?\\)::(.*)");
+
+        Matcher matcher = pattern.matcher(tokenResponse.getName());
+        if (matcher.matches()) {
+            return Optional.of(matcher.group(matcher.groupCount()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void run() {
+        try (ClientSessionFactory sessionFactory = serverLocator.createSessionFactory();
+             ClientSession session = sessionFactory.createSession();
+             ClientConsumer consumer = session.createConsumer(ArtemisSupervisor.REGISTER_TOPIC)) {
+            session.start();
+            while (running.get()) {
+                consume(consumer, session);
+            }
+        } catch (Exception e) {
+            log.warn("Closing consumer", e);
+            running.set(false);
+        }
+    }
+
+    @Override
+    public void close() {
+        log.info("Stopping ArtemisTokenRegistrar");
+        running.set(false);
+    }
+
+    /**
+     * Execute the http call. If it is successful ack the message.
+     * <p>
+     * - rollback if an exception is thrown (probably communicating with the server)
+     * - rollback if we receive a 5xx
+     * - ack if we receive a 401,403,409, etc
+     * <p>
+     * We should probably return an enum instead of a boolean to really encapsulate the intentions
+     * of the return value
+     *
+     * @param call          the http call to execute
+     * @param tokenResponse the ACE Token Response to upload
+     * @return if the current transaction should be rolled back
+     */
+    private boolean accept(Call<AceTokenModel> call, TokenResponse tokenResponse) {
+        log.debug("[{}] Attempting to register token at {}",
+                tokenResponse.getName(), call.request().url().toString());
+        boolean rollback = false;
+
+        try {
+            Response<AceTokenModel> response = call.execute();
+            int responseCode = response.code();
+            if (response.isSuccessful() || responseCode == 409) {
+                log.info("AceToken registered with the Ingest Server");
+            } else {
+                log.warn("Unable to register AceToken! Response Code is {}", responseCode);
+            }
+
+            if (responseCode >= 500) {
+                rollback = true;
+                log.warn("Server error ({}), session should be rolled back", responseCode);
+            }
+        } catch (IOException e) {
+            rollback = true;
+            log.warn("Error communicating with server, session should be rolled back", e);
+        }
+
+        return rollback;
+    }
+}
---
+++ tokenizer-mq/src/main/java/org/chronopolis/tokenize/mq/artemis/ArtemisTokenRequest.java
@@ -0,0 +1,191 @@
+package org.chronopolis.tokenize.mq.artemis;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import edu.umiacs.ace.ims.ws.TokenRequest;
+import edu.umiacs.ace.ims.ws.TokenResponse;
+import org.apache.activemq.artemis.api.core.ActiveMQException;
+import org.apache.activemq.artemis.api.core.client.ClientConsumer;
+import org.apache.activemq.artemis.api.core.client.ClientMessage;
+import org.apache.activemq.artemis.api.core.client.ClientSession;
+import org.apache.activemq.artemis.api.core.client.ClientSessionFactory;
+import org.apache.activemq.artemis.api.core.client.ServerLocator;
+import org.chronopolis.rest.models.Bag;
+import org.chronopolis.tokenize.ManifestEntry;
+import org.chronopolis.tokenize.batch.ImsServiceWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Proto implementation for a TokenRequestBatch using ActiveMQ Artemis to receive requests
+ *
+ * @author shake
+ */
+public class ArtemisTokenRequest implements Runnable, Closeable {
+
+    private final Logger log = LoggerFactory.getLogger(ArtemisTokenRequest.class);
+
+    private TimeUnit unit = TimeUnit.MILLISECONDS;
+
+    private final AtomicBoolean running = new AtomicBoolean(true);
+
+    private final ImsServiceWrapper ims;
+    private final ArtemisSupervisor supervisor;
+    private final ServerLocator serverLocator;
+
+    public ArtemisTokenRequest(ImsServiceWrapper ims,
+                               ArtemisSupervisor supervisor,
+                               ServerLocator serverLocator) {
+        this.ims = ims;
+        this.supervisor = supervisor;
+        this.serverLocator = serverLocator;
+    }
+
+    /**
+     * Capture n messages where n < batchSize and use them to create a TokenRequest to the ACE IMS
+     *
+     * @param consumer the MQ consumer to retrieve messages with
+     */
+    private void consume(ClientConsumer consumer) {
+        Map<String, ClientMessage> receivedMessages = new HashMap<>();
+        final Long timeout = (long) ims.configuration().getWaitTime();
+        final int batchSize = ims.configuration().getQueueLength();
+        final long deadline = System.nanoTime() + unit.toNanos(timeout);
+        long localTimeout = deadline;
+
+        try {
+            // I'm not the biggest fan of constantly polling for messages, but since we can't ask
+            // the broker for a batch it seems to be the best we can do
+            while (receivedMessages.size() < batchSize && localTimeout > 0) {
+                long l = TimeUnit.NANOSECONDS.toMillis(localTimeout);
+                ClientMessage received;
+                received = consumer.receive(l);
+                if (received != null) {
+                    String id = received.getStringProperty("id");
+                    if (id != null && !id.isEmpty()) {
+                        received.individualAcknowledge();
+                        receivedMessages.put(id, received);
+                    }
+                }
+
+                localTimeout = deadline - System.nanoTime();
+            }
+
+            if (!receivedMessages.isEmpty()) {
+                log.info("[Tokenizer] Processing {} messages", receivedMessages.size());
+                processMessages(receivedMessages);
+            }
+
+            // session.commit();
+        } catch (ActiveMQException e) {
+            log.warn("Error with ActiveMQ", e);
+        }
+    }
+
+    private void processMessages(Map<String, ClientMessage> receivedMessages) {
+        final ObjectMapper mapper = new ObjectMapper();
+        final SimpleModule module = new SimpleModule();
+        module.addDeserializer(ManifestEntry.class, new ManifestEntryDeserializer());
+        mapper.registerModule(module);
+
+        final List<TokenRequest> requests = new ArrayList<>();
+        final Map<String, ManifestEntry> entries = new HashMap<>();
+
+        // Deserialize and create a TokenRequest for each message we received
+        for (ClientMessage message : receivedMessages.values()) {
+            try {
+                String body = message.getReadOnlyBodyBuffer().readString();
+                ManifestEntry entry = mapper.readValue(body, ManifestEntry.class);
+                requests.add(createRequest(entry));
+                entries.put(entry.tokenName(), entry);
+            } catch (JsonParseException e) {
+                log.error("Invalid json", e);
+            } catch (IOException e) {
+                log.error("Error parsing message", e);
+            }
+        }
+
+        // will probably want to revisit how we handle exceptions here
+        // notably because communication failure with the ims should just involve rolling back
+        // messages instead of just acking them
+        // boolean interrupted = false;
+        String tokenClass = ims.configuration().getTokenClass();
+        try {
+            List<TokenResponse> tokens = ims.requestTokensImmediate(tokenClass, requests);
+            for (TokenResponse token : tokens) {
+                ManifestEntry entry = entries.remove(token.getName());
+                supervisor.associate(entry, token);
+            }
+        } catch (Exception e) {
+            log.info("Error communicating with the ims", e);
+        }
+    }
+
+    private TokenRequest createRequest(ManifestEntry entry) {
+        TokenRequest request = new TokenRequest();
+        request.setHashValue(entry.getRegisteredDigest());
+        request.setName(entry.tokenName());
+        return request;
+    }
+
+    // Basic methods for running continuously and triggering a stop
+
+    @Override
+    public void run() {
+        try (ClientSessionFactory sessionFactory = serverLocator.createSessionFactory();
+             ClientSession session = sessionFactory.createSession();
+             ClientConsumer consumer = session.createConsumer(ArtemisSupervisor.REQUEST_TOPIC)) {
+            session.start();
+            while (running.get()) {
+                consume(consumer);
+            }
+        } catch (Exception e) {
+            log.warn("Closing consumer", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        log.info("Stopping ArtemisTokenRequest");
+        running.set(false);
+    }
+
+    public class ManifestEntryDeserializer extends StdDeserializer<ManifestEntry> {
+
+        ManifestEntryDeserializer() {
+            this(null);
+        }
+
+        ManifestEntryDeserializer(Class<?> clazz) {
+            super(clazz);
+        }
+
+        @Override
+        public ManifestEntry deserialize(JsonParser p, DeserializationContext ctxt)
+                throws IOException {
+            ObjectCodec codec = p.getCodec();
+            JsonNode treeNode = codec.readTree(p);
+
+            JsonNode bagNode = treeNode.get("bag");
+            Bag bag = codec.readValue(bagNode.traverse(), Bag.class);
+            JsonNode pathNode = treeNode.get("path");
+            JsonNode registeredDigest = treeNode.get("registeredDigest");
+            return new ManifestEntry(bag, pathNode.asText(), registeredDigest.asText());
+        }
+    }
+}
