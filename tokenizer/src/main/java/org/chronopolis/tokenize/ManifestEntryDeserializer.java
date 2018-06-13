576204947957cf79bc4eccaf7f8061ed698a8697
Michael Ritter <shake@umiacs.umd.edu>
2018-06-13 13:40:22 -0400

    Deserializer for ManifestEntry

---
+++ tokenizer/src/main/java/org/chronopolis/tokenize/ManifestEntryDeserializer.java
@@ -0,0 +1,32 @@
+package org.chronopolis.tokenize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.chronopolis.rest.models.Bag;
+
+import java.io.IOException;
+
+/**
+ * Jackson Deserializer for a {@link ManifestEntry}. This only populates the bag, path, and
+ * registeredDigest fields.
+ *
+ * @author shake
+ */
+public class ManifestEntryDeserializer extends JsonDeserializer<ManifestEntry> {
+
+    @Override
+    public ManifestEntry deserialize(JsonParser p, DeserializationContext ctxt)
+            throws IOException {
+        ObjectCodec codec = p.getCodec();
+        JsonNode treeNode = codec.readTree(p);
+
+        JsonNode bagNode = treeNode.get("bag");
+        Bag bag = codec.readValue(bagNode.traverse(), Bag.class);
+        JsonNode pathNode = treeNode.get("path");
+        JsonNode registeredDigest = treeNode.get("registeredDigest");
+        return new ManifestEntry(bag, pathNode.asText(), registeredDigest.asText());
+    }
+}
