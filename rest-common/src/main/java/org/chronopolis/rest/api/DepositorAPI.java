a1e45af37337cf7817af7f9a657ff0293d275394
Michael Ritter <shake@umiacs.umd.edu>
2018-08-10 16:43:26 -0400

    #105 Add QueryMap for passing parameters

--- rest-common/src/main/java/org/chronopolis/rest/api/DepositorAPI.java
+++ rest-common/src/main/java/org/chronopolis/rest/api/DepositorAPI.java
@@ -14,6 +14,9 @@ import retrofit2.http.GET;
 import retrofit2.http.HEAD;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
+
+import java.util.Map;
 
 import static org.chronopolis.rest.api.Paths.DEPOSITOR_ROOT;
 
@@ -30,7 +33,7 @@ public interface DepositorAPI {
      * @return all depositors
      */
     @GET(DEPOSITOR_ROOT)
-    Call<PageImpl<DepositorModel>> getDepositors();
+    Call<PageImpl<DepositorModel>> getDepositors(@QueryMap Map<String, String> params);
 
     /**
      * Create a depositor
