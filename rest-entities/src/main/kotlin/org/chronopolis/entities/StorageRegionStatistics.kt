1b948e8e5c56d0d7485a8900b4df042a064615dc
Michael Ritter <shake@umiacs.umd.edu>
2019-04-02 17:01:16 -0400

    Implement StorageRegion usedSpace

---
+++ rest-entities/src/main/kotlin/org/chronopolis/entities/StorageRegionStatistics.kt
@@ -0,0 +1,21 @@
+package org.chronopolis.entities
+
+import org.chronopolis.entities.tables.StorageRegion
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
+import java.math.BigDecimal
+
+
+/**
+ * Get the amount of space used by active [StagingStorage] for a [StorageRegion]
+ *
+ * @since 3.2.0
+ * @author shake
+ */
+fun usedSpace(ctx: DSLContext, region: StorageRegion): BigDecimal {
+    val staging = Tables.STAGING_STORAGE
+    return ctx.select(DSL.sum(staging.SIZE))
+            .from(staging)
+            .where(staging.REGION_ID.eq(region.ID).and(staging.ACTIVE.isTrue))
+            .fetchOne(0, BigDecimal::class.java)
+}
