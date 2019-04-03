f199f8aa9824ff20b2611be0039e4de8fc76bdf1
Michael Ritter <shake@umiacs.umd.edu>
2019-04-03 15:03:57 -0400

    Skeletons for testing other functions

---
+++ rest-entities/src/test/java/org/chronopolis/db/queries/BagStatisticsTest.java
@@ -0,0 +1,31 @@
+package org.chronopolis.db.queries;
+
+import org.chronopolis.rest.entities.JPAContext;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author shake
+ */
+@DataJpaTest(excludeAutoConfiguration = FlywayAutoConfiguration.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = JPAContext.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BagStatisticsTest {
+
+    @Test
+    @Ignore
+    public void testStatsByGroup() {
+    }
+
+    @Test
+    @Ignore
+    public void testOverview() {
+    }
+}
---
+++ rest-entities/src/test/java/org/chronopolis/db/queries/DepositorStatisticsTest.java
@@ -0,0 +1,36 @@
+package org.chronopolis.db.queries;
+
+import org.chronopolis.rest.entities.JPAContext;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author shake
+ */
+@DataJpaTest(excludeAutoConfiguration = FlywayAutoConfiguration.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = JPAContext.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DepositorStatisticsTest {
+
+    @Test
+    @Ignore
+    public void testDepositorSummary() {
+    }
+
+    @Test
+    @Ignore
+    public void testTopDepositorsBySum() {
+    }
+
+    @Test
+    @Ignore
+    public void testTopDepositorsByCount() {
+    }
+}
