b3303aa31f284eeb09b4fef6573334fe54ce960a
Michael Ritter <shake@umiacs.umd.edu>
2019-05-01 13:36:38 -0400

    Remove extraneous annotation

--- rest-entities/src/test/java/org/chronopolis/db/queries/StorageRegionQueriesTest.kt
+++ rest-entities/src/test/java/org/chronopolis/db/queries/StorageRegionQueriesTest.kt
@@ -14,7 +14,6 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import java.math.BigDecimal
 
-@DataJpaTest(excludeAutoConfiguration = [FlywayAutoConfiguration::class])
 @RunWith(SpringRunner::class)
 @ContextConfiguration(classes = [JPAContext::class])
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
